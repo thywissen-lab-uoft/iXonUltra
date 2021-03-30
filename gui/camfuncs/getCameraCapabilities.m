af5ca3deaee1fdd40d1eb57ebeaa21b159d90ae9
Cora <63414958+cjfujiwara@users.noreply.github.com>
2021-03-29 22:39:20 -0400

    finishing cameracapabilities

--- gui/camfuncs/getCameraCapabilities.m
+++ gui/camfuncs/getCameraCapabilities.m
@@ -38,41 +38,8 @@ fprintf('Reading camera capabilities ... ');
 
 nums=[int32_ulAcqModes,int32_ulReadModes,int32_ulFTReadModes,...
     int32_ulTriggerModes,int32_ulCameraType,int32_ulPixelModes,int32_ulSetFunctions,...
-    int32_ulGetFunctions,int32_ulFeatures,int32_ulPCICard,int32_ulEMGainCapability]
-%{
-[ret,...
-   c01,...
-   c02,...
-   c03,...
-   c04,...
-   c05,...
-   c06,...
-   c07,...
-   c08,...
-   c09,...
-   c10,...
-   c11]=GetCapabilities;
+    int32_ulGetFunctions,int32_ulFeatures,int32_ulPCICard,int32_ulEMGainCapability];
 
-%a=0;
-%b=0;
-%c=0;
-%[ret, QE] = andorfunctions('GetQE',
-
-disp('raw output');
-disp(ret)
-disp(dec2bin(c01,32));
-disp(dec2bin(c02,32));
-disp(dec2bin(c03,32));
-disp(dec2bin(c04,32));
-disp(dec2bin(c05,32));
-disp(dec2bin(c06,32));
-disp(dec2bin(c07,32));
-disp(dec2bin(c08,32));
-disp(dec2bin(c09,32));
-disp(dec2bin(c10,32));
-disp(dec2bin(c11,32));
-disp(' ')
-   %}
 % Note that MATLAB is 1 indexed while the codes are 0 indexed
 
 
@@ -87,46 +54,46 @@ end
 
 %% AcqModes
 ulAcqModes=struct;
-ulAcqModes.AC_ACQMODE_SINGLE=bitget(int32_ulAcqModes,1);
-ulAcqModes.AC_ACQMODE_VIDEO=bitget(int32_ulAcqModes,2);
-ulAcqModes.AC_ACQMODE_ACCUMULATE=bitget(int32_ulAcqModes,3);
-ulAcqModes.AC_ACQMODE_KINETIC=bitget(int32_ulAcqModes,4);
-ulAcqModes.AC_ACQMODE_FRAMETRANSFER=bitget(int32_ulAcqModes,5);
-ulAcqModes.AC_ACQMODE_FASTKINETICS=bitget(int32_ulAcqModes,6);
-ulAcqModes.AC_ACQMODE_OVERLAP=bitget(int32_ulAcqModes,7);
+ulAcqModes.AC_ACQMODE_SINGLE=bitget(int32_ulAcqModes,0+1);
+ulAcqModes.AC_ACQMODE_VIDEO=bitget(int32_ulAcqModes,1+1);
+ulAcqModes.AC_ACQMODE_ACCUMULATE=bitget(int32_ulAcqModes,2+1);
+ulAcqModes.AC_ACQMODE_KINETIC=bitget(int32_ulAcqModes,3+1);
+ulAcqModes.AC_ACQMODE_FRAMETRANSFER=bitget(int32_ulAcqModes,4+1);
+ulAcqModes.AC_ACQMODE_FASTKINETICS=bitget(int32_ulAcqModes,5+1);
+ulAcqModes.AC_ACQMODE_OVERLAP=bitget(int32_ulAcqModes,6+1);
 
 cam_skills.ulAcqModes=ulAcqModes;
 %% Read Modes
 ulReadModes=struct;
-ulReadModes.AC_READMODE_FULLIMAGE=bitget(int32_ulReadModes,1);
-ulReadModes.AC_READMODE_SUBIMAGE=bitget(int32_ulReadModes,2);
-ulReadModes.AC_READMODE_SINGLETRACK=bitget(int32_ulReadModes,3);
-ulReadModes.AC_READMODE_FVB=bitget(int32_ulReadModes,4);
-ulReadModes.AC_READMODE_MULTITRACK=bitget(int32_ulReadModes,5);
-ulReadModes.AC_READMODE_RANDOMTRACK=bitget(int32_ulReadModes,6);
+ulReadModes.AC_READMODE_FULLIMAGE=bitget(int32_ulReadModes,0+1);
+ulReadModes.AC_READMODE_SUBIMAGE=bitget(int32_ulReadModes,1+1);
+ulReadModes.AC_READMODE_SINGLETRACK=bitget(int32_ulReadMode,2+1);
+ulReadModes.AC_READMODE_FVB=bitget(int32_ulReadModes,3+1);
+ulReadModes.AC_READMODE_MULTITRACK=bitget(int32_ulReadModes,4+1);
+ulReadModes.AC_READMODE_RANDOMTRACK=bitget(int32_ulReadModes,5+1);
 
 cam_skills.ulReadModes=ulReadModes;
 %% Frame Transfer Read Modes
 ulFTReadModes=struct;
-ulFTReadModes.AC_READMODE_FULLIMAGE=bitget(int32_ulFTReadModes,1);
-ulFTReadModes.AC_READMODE_SUBIMAGE=bitget(int32_ulFTReadModes,2);
-ulFTReadModes.AC_READMODE_SINGLETRACK=bitget(int32_ulFTReadModes,3);
-ulFTReadModes.AC_READMODE_FVB=bitget(int32_ulFTReadModes,4);
-ulFTReadModes.AC_READMODE_MULTITRACK=bitget(int32_ulFTReadModes,5);
-ulFTReadModes.AC_READMODE_RANDOMTRACK=bitget(int32_ulFTReadModes,6);
+ulFTReadModes.AC_READMODE_FULLIMAGE=bitget(int32_ulFTReadModes,0+1);
+ulFTReadModes.AC_READMODE_SUBIMAGE=bitget(int32_ulFTReadModes,1+1);
+ulFTReadModes.AC_READMODE_SINGLETRACK=bitget(int32_ulFTReadModes,2+1);
+ulFTReadModes.AC_READMODE_FVB=bitget(int32_ulFTReadModes,3+1);
+ulFTReadModes.AC_READMODE_MULTITRACK=bitget(int32_ulFTReadModes,4+1);
+ulFTReadModes.AC_READMODE_RANDOMTRACK=bitget(int32_ulFTReadModes,5+1);
 
 cam_skills.ulFTReadModes=ulFTReadModes;
 %% Trigger Modes
 ulTriggerModes=struct;
-ulTriggerModes.AC_TRIGGERMODE_INTERNAL=bitget(int32_ulTriggerModes,1);
-ulTriggerModes.AC_TRIGGERMODE_EXTERNAL=bitget(int32_ulTriggerModes,2);
-ulTriggerModes.AC_TRIGGERMODE_EXTERNAL_FVB_EM=bitget(int32_ulTriggerModes,3);
-ulTriggerModes.AC_TRIGGERMODE_CONTINUOUS=bitget(int32_ulTriggerModes,4);
-ulTriggerModes.AC_TRIGGERMODE_EXTERNALSTART=bitget(int32_ulTriggerModes,5);
-%     ulTriggerModes.AC_TRIGGERMODE_BULB=bitget(int32_ulTriggerModes,6); depreciated
-ulTriggerModes.AC_TRIGGERMODE_EXTERNALEXPOSURE=bitget(int32_ulTriggerModes,6);
-ulTriggerModes.AC_TRIGGERMODE_INVERTED=bitget(int32_ulTriggerModes,7);
-ulTriggerModes.AC_TRIGGERMODE_EXTERNAL_CHARGESHIFTING=bitget(int32_ulTriggerModes,8);
+ulTriggerModes.AC_TRIGGERMODE_INTERNAL=bitget(int32_ulTriggerModes,0+1);
+ulTriggerModes.AC_TRIGGERMODE_EXTERNAL=bitget(int32_ulTriggerModes,1+1);
+ulTriggerModes.AC_TRIGGERMODE_EXTERNAL_FVB_EM=bitget(int32_ulTriggerModes,2+1);
+ulTriggerModes.AC_TRIGGERMODE_CONTINUOUS=bitget(int32_ulTriggerModes,3+1);
+ulTriggerModes.AC_TRIGGERMODE_EXTERNALSTART=bitget(int32_ulTriggerModes,4+1);
+%     ulTriggerModes.AC_TRIGGERMODE_BULB=bitget(int32_ulTriggerModes,5+1); depreciated
+ulTriggerModes.AC_TRIGGERMODE_EXTERNALEXPOSURE=bitget(int32_ulTriggerModes,5+1);
+ulTriggerModes.AC_TRIGGERMODE_INVERTED=bitget(int32_ulTriggerModes,6+1);
+ulTriggerModes.AC_TRIGGERMODE_EXTERNAL_CHARGESHIFTING=bitget(int32_ulTriggerModes,7+1);
 
 cam_skills.ulTriggerModes=ulTriggerModes;
 %% CameraType
@@ -144,16 +111,101 @@ ulCameraType=camtypes{int32_ulCameraType+1};
 cam_skills.ulCameraType=ulCameraType;
 %% PixelModes
 ulPixelModes=struct;
-ulPixelModes.AC_PIXELMODE_8BIT=bitget(int32_ulPixelModes,1);
-ulPixelModes.AC_PIXELMODE_14BIT=bitget(int32_ulPixelModes,2);
-ulPixelModes.AC_PIXELMODE_16BIT=bitget(int32_ulPixelModes,3);
-ulPixelModes.AC_PIXELMODE_32BIT=bitget(int32_ulPixelModes,4);
+ulPixelModes.AC_PIXELMODE_8BIT=bitget(int32_ulPixelModes,0+1);
+ulPixelModes.AC_PIXELMODE_14BIT=bitget(int32_ulPixelModes,1+1);
+ulPixelModes.AC_PIXELMODE_16BIT=bitget(int32_ulPixelModes,2+1);
+ulPixelModes.AC_PIXELMODE_32BIT=bitget(int32_ulPixelModes,3+1);
 
 pxmodes={'AC_PIXELMODE_MONO','AC_PIXELMODE_RGB','AC_PIXELMODE_CMY'};
 
 ind=bin2dec(strtrim(num2str(bitget(int32_ulPixelModes,17:32))))+1;
 
 cam_skills.ulPixelModes=pxmodes{ind};
+
+%% ulSetFunctions
+ulSetFunctions=struct;
+ulSetFunctions.AC_SETFUNCTION_VREADOUT=bitget(int32_ulSetFunctions,0+1);
+ulSetFunctions.AC_SETFUNCTION_HREADOUT=bitget(int32_ulSetFunctions,1+1);
+ulSetFunctions.AC_SETFUNCTION_TEMPERATURE=bitget(int32_ulSetFunctions,2+1);
+ulSetFunctions.AC_SETFUNCTION_MCPGAIN=bitget(int32_ulSetFunctions,3+1);
+ulSetFunctions.AC_SETFUNCTION_EMCCDGAIN=bitget(int32_ulSetFunctions,4+1);
+ulSetFunctions.AC_SETFUNCTION_BASELINECLAMP=bitget(int32_ulSetFunctions,5+1);
+ulSetFunctions.AC_SETFUNCTION_VSAMPLITUDE=bitget(int32_ulSetFunctions,6+1);
+ulSetFunctions.AC_SETFUNCTION_HIGHCAPACITY=bitget(int32_ulSetFunctions,7+1);
+
+ulSetFunctions.AC_SETFUNCTION_BASELINEOFFSET=bitget(int32_ulSetFunctions,8+1);
+ulSetFunctions.AC_SETFUNCTION_PREAMPGAIN=bitget(int32_ulSetFunctions,9+1);
+ulSetFunctions.AC_SETFUNCTION_CROPMODE=bitget(int32_ulSetFunctions,10+1);
+ulSetFunctions.AC_SETFUNCTION_DMAPARAMETERS=bitget(int32_ulSetFunctions,11+1);
+ulSetFunctions.AC_SETFUNCTION_HORIZONTALBIN=bitget(int32_ulSetFunctions,12+1);
+ulSetFunctions.AC_SETFUNCTION_MULTITRACKHRANGE=bitget(int32_ulSetFunctions,13+1);
+ulSetFunctions.AC_SETFUNCTION_RANDOMTRACKNOGAPS=bitget(int32_ulSetFunctions,14+1);
+ulSetFunctions.AC_SETFUNCTION_EMADVANCED=bitget(int32_ulSetFunctions,15+1);
+
+ulSetFunctions.AC_SETFUNCTION_GATEMODE=bitget(int32_ulSetFunctions,16+1);
+ulSetFunctions.AC_SETFUNCTION_DDGTIMES=bitget(int32_ulSetFunctions,17+1);
+ulSetFunctions.AC_SETFUNCTION_IOC=bitget(int32_ulSetFunctions,18+1);
+ulSetFunctions.AC_SETFUNCTION_INTELLIGATE=bitget(int32_ulSetFunctions,19+1);
+ulSetFunctions.AC_SETFUNCTION_INSERTION_DELAY=bitget(int32_ulSetFunctions,20+1);
+ulSetFunctions.AC_SETFUNCTION_GATESTEP=bitget(int32_ulSetFunctions,21+1);
+ulSetFunctions.AC_SETFUNCTION_TRIGGERTERMINATION=bitget(int32_ulSetFunctions,22+1);
+ulSetFunctions.AC_SETFUNCTION_EXTENDEDNIR=bitget(int32_ulSetFunctions,23+1);
+ulSetFunctions.AC_SETFUNCTION_SPOOLTHREADCOUNT=bitget(int32_ulSetFunctions,24+1);
+
+% In newer version of SDK?
+% ulSetFunctions.AC_SETFUNCTION_REGISTERPACK=bitget(int32_ulSetFunctions,25+1);
+% ulSetFunctions.AC_SETFUNCTION_PRESCANS=bitget(int32_ulSetFunctions,26+1);
+% ulSetFunctions.AC_SETFUNCTION_GATEWIDTHSTEP=bitget(int32_ulSetFunctions,27+1);
+% ulSetFunctions.AC_SETFUNCTION_EXTENDED_CROP_MODE=bitget(int32_ulSetFunctions,28+1);
+
+cam_skills.ulSetFunctions=ulSetFunctions;
+
+%% ulGetFunctions
+ulGetFunctions=struct;
+ulGetFunctions.AC_GETFUNCTION_TEMPERATURE=bitget(int32_ulGetFunctions,0+1);
+ulGetFunctions.AC_GETFUNCTION_TEMPERATURERANGE=bitget(int32_ulGetFunctions,2+1);
+ulGetFunctions.AC_GETFUNCTION_DETECTORSIZE=bitget(int32_ulGetFunctions,3+1);
+ulGetFunctions.AC_GETFUNCTION_MCPGAIN=bitget(int32_ulGetFunctions,4+1);
+ulGetFunctions.AC_GETFUNCTION_EMCCDGAIN=bitget(int32_ulGetFunctions,5+1);
+ulGetFunctions.AC_GETFUNCTION_GATEMODE=bitget(int32_ulGetFunctions,7+1);
+ulGetFunctions.AC_GETFUNCTION_DDGTIMES=bitget(int32_ulGetFunctions,8+1);
+ulGetFunctions.AC_GETFUNCTION_IOC=bitget(int32_ulGetFunctions,9+1);
+
+ulGetFunctions.AC_GETFUNCTION_INTELLIGATE=bitget(int32_ulGetFunctions,10+1);
+ulGetFunctions.AC_GETFUNCTION_INSERTION_DELAY=bitget(int32_ulGetFunctions,11+1);
+ulGetFunctions.AC_GETFUNCTION_PHOSPHORSTATUS=bitget(int32_ulGetFunctions,13+1);
+ulGetFunctions.AC_GETFUNCTION_BASELINECLAMP=bitget(int32_ulGetFunctions,15+1);
+
+cam_skills.ulGetFunctions=ulGetFunctions;
+
+%% ulFeatures
+ulFeatures=struct;
+ulFeatures.AC_FEATURES_POLLING=bitget(int32_ulFeatures,0+1);
+ulFeatures.AC_FEATURES_EVENTS=bitget(int32_ulFeatures,1+1);
+ulFeatures.AC_FEATURES_SPOOLING=bitget(int32_ulFeatures,2+1);
+ulFeatures.AC_FEATURES_SHUTTER=bitget(int32_ulFeatures,3+1);
+ulFeatures.AC_FEATURES_SHUTTEREX=bitget(int32_ulFeatures,4+1);
+ulFeatures.AC_FEATURES_EXTERNAL_I2C=bitget(int32_ulFeatures,5+1);
+ulFeatures.AC_FEATURES_SATURATIONEVENT=bitget(int32_ulFeatures,6+1);
+
+ulFeatures.AC_FEATURES_FANCONTROL=bitget(int32_ulFeatures,7+1);
+ulFeatures.AC_FEATURES_MIDFANCONTROL=bitget(int32_ulFeatures,8+1);
+ulFeatures.AC_FEATURES_TEMPERATUREDURINGACQUISITION=bitget(int32_ulFeatures,9+1);
+ulFeatures.AC_FEATURES_KEEPCLEANCONTROL=bitget(int32_ulFeatures,10+1);
+ulFeatures.AC_FEATURES_DDGLITE=bitget(int32_ulFeatures,11+1);
+ulFeatures.AC_FEATURES_FTEXTERNALEXPOSURE=bitget(int32_ulFeatures,12+1);
+ulFeatures.AC_FEATURES_KINETICEXTERNALEXPOSURE=bitget(int32_ulFeatures,13+1);
+ulFeatures.AC_FEATURES_DACCONTROL=bitget(int32_ulFeatures,14+1);
+
+ulFeatures.AC_FEATURES_METADATA=bitget(int32_ulFeatures,15+1);
+ulFeatures.AC_FEATURES_IOCONTROL=bitget(int32_ulFeatures,16+1);
+ulFeatures.AC_FEATURES_PHOTONCOUNTING=bitget(int32_ulFeatures,17+1);
+ulFeatures.AC_FEATURES_COUNTCONVERT=bitget(int32_ulFeatures,18+1);
+ulFeatures.AC_FEATURES_DUALMODE=bitget(int32_ulFeatures,19+1);
+
+cam_skills.ulFeatures=ulFeatures;
+
+%%
 
 %% PCI Card
 ulPCICard=int32_ulPCICard;
