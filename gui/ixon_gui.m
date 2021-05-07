9769eca16311012fe627b6f47d3a35d4c2cd23cb
Cora <63414958+cjfujiwara@users.noreply.github.com>
2021-05-07 17:51:15 -0400

    using analysis box count for gui

--- gui/ixon_gui.m
+++ gui/ixon_gui.m
@@ -1585,7 +1585,7 @@ function data=updateImages(data)
     Zsub=data.Z(y,x);
     
     % Perform Box Count ALWAYS DONE
-    data=boxCount(data);
+    data=ixon_boxCount(data);
     bc=data.BoxCount;
     
     % Box counts analysis table string
@@ -2139,65 +2139,6 @@ function out=softwareTrigger
         out=0;
     end
 end
-
-
-
-%% Analysis Functions
-
-function dstruct=boxCount(dstruct,bgROI)
-    fprintf('Performing box count analysis ...');   
-    
-    if nargin==1
-        bgROI=NaN;
-    end
-  
-    
-    BoxCount=struct;    
-    for k=1:size(dstruct.ROI,1)
-        ROI=dstruct.ROI(k,:);
-        x=dstruct.X(ROI(1):ROI(2));                 % X vector
-        y=dstruct.Y(ROI(3):ROI(4));                 % Y vector
-        z=double(dstruct.Z(ROI(3):ROI(4),ROI(1):ROI(2)));
-        nbg=0;
-        
-        if nargin==2
-            zbg=double(dstruct.Z(bgROI(3):bgROI(4),bgROI(1):bgROI(2)));
-            nbg=sum(sum(zbg))/(size(zbg,1)*size(zbg,2)); % count density
-        end 
-        
-        Nraw=sum(sum(z));
-        Nbg=nbg*size(z,1)*size(z,2);  
-        
-        zNoBg=z-nbg;        
-        Ncounts=sum(sum(zNoBg));   
-        zY=sum(zNoBg,2)';
-        zX=sum(zNoBg,1);
-               
-        % Calculate center of mass
-        Xc=sum(zX.*x)/Ncounts;
-        Yc=sum(zY.*y)/Ncounts;
-        
-        % Calculate central second moment/variance and the standard
-        % deviation
-        X2=sum(zX.*(x-Xc).^2)/Ncounts; % x variance
-        Xs=sqrt(X2); % standard deviation X
-        Y2=sum(zY.*(y-Yc).^2)/Ncounts; % x variance
-        Ys=sqrt(Y2); % standard deviation Y               
-
-        BoxCount(k).Ncounts=Ncounts;    % Number of counts (w/ bkgd removed)
-        BoxCount(k).Nraw=Nraw;          % Raw of number of counts
-        BoxCount(k).Nbkgd=Nbg;          % Bakcground number of counts
-        BoxCount(k).nbkgd=nbg;          % Background counts/px
-        BoxCount(k).bgROI=bgROI;        % ROI for calculating bgkd
-        BoxCount(k).Xc=Xc;              % X center of mass
-        BoxCount(k).Yc=Yc;              % Y center of mass
-        BoxCount(k).Xs=Xs;              % X standard deviation
-        BoxCount(k).Ys=Ys;              % Y standard deviation
-    end    
-    dstruct.BoxCount=BoxCount;
-    disp('done');
-end
-  
 
 
 %% HELPER
