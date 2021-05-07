07857f97b00c9e7c0da4bf703315d9a823f7b66e
Cora <63414958+cjfujiwara@users.noreply.github.com>
2021-05-07 18:05:07 -0400

    gauss to analysis
    moved gaussian fit to analysis section and made comptabitble. Can I inpororate rotations into one function?

--- gui/ixon_gui.m
+++ gui/ixon_gui.m
@@ -837,24 +837,34 @@ acqTimer=timer('Name','iXonAcquisitionWatchTimer','Period',.5,...
 %% Image Process Panel
 
 hpADV=uipanel(hF,'units','pixels','backgroundcolor','w',...
-    'Position',[0 hpAcq.Position(2)-60 160 80],'title','processing');
+    'Position',[0 hpAcq.Position(2)-60 160 100],'title','processing');
 
 % Checkbox for applying point spread function (should this be a separate
 % panel?)
+ttstr='Sharpen image using point spread function';
 hcPSF=uicontrol(hpADV,'style','checkbox','string','sharpen w/ PSF','fontsize',8,...
     'backgroundcolor','w','Position',[5 0 100 20],'callback',@() disp('hi'),...
     'ToolTipString',ttstr,'enable','off');
 
 % Checkbox for new processings
-hcMask=uicontrol(hpADV,'style','checkbox','string','image mask','fontsize',8,...
-    'backgroundcolor','w','Position',[5 20 100 20],'callback',@(~,~) disp('hi'),...
+ttstr='Apply mask to image to eliminate aperture clipping';
+hcMask=uicontrol(hpADV,'style','checkbox','string','apply image mask','fontsize',8,...
+    'backgroundcolor','w','Position',[5 20 120 20],...
     'ToolTipString',ttstr,'enable','on','Value',1);
 
+% Checkbox for new processings
+ttstr='Subtract off electronic/software bias of 200 counts from raw images.';
+hcSubBias=uicontrol(hpADV,'style','checkbox','string','subtract bias','fontsize',8,...
+    'backgroundcolor','w','Position',[5 40 100 20],...
+    'ToolTipString',ttstr,'enable','on','Value',1);
+
+
 % Checkbox for enabling 2D gauss fitting
+ttstr='Apply gaussian filter to smooth image';
 cGaussFilter=uicontrol('style','checkbox','string','gauss filter',...
     'units','pixels','parent',hpADV,'backgroundcolor','w',...
-    'value',1);
-cGaussFilter.Position=[5 40 80 20];
+    'value',1,'ToolTipString',ttstr);
+cGaussFilter.Position=[5 60 80 20];
 
 tblGaussFilter=uitable('parent',hpADV,'units','pixels',...
     'rowname',{},'columnname',{},'Data',.25,'columneditable',[true],...
@@ -1564,6 +1574,12 @@ function data=updateImages(data)
     
     imgs=data.RawImages;
     
+    if hcSubBias.Value
+        for p=1:size(imgs,3)
+            imgs(:,:,p)=imgs(:,:,p)-200;
+        end
+    end
+    
     if hcMask.Value
         disp('Applying mask.');
         for p=1:size(imgs,3)
@@ -1737,7 +1753,15 @@ end
 function data=updateAnalysis(data)
     if hcGauss.Value
         disp('Fitting data to 2D gaussian...')
-        data=gaussFit(data);            
+        
+        
+        opts=struct;
+        opts.doRescale=1;
+        opts.doMask=hcMask.Value;
+        opts.Scale=0.5;
+        opts.Mask=ixon_mask;
+        
+        data=ixon_gaussFit(data,opts);            
         cGaussRet.Enable='on';
         updateGaussPlot(data);
     end
@@ -1847,90 +1871,6 @@ end
 %% Analysis Funciton
 
 
-function data=gaussFit(data,sc)
-
-    if nargin==1
-        sc=.5;
-    end
-    GaussFit={};    
-    for k=1:size(data.ROI,1)
-        ROI=data.ROI(k,:);
-
-        % Acquire the data
-        X=double(data.X(ROI(1):ROI(2)));
-        Y=double(data.Y(ROI(3):ROI(4)));
-        Zdata=double(data.Z(ROI(3):ROI(4),ROI(1):ROI(2)));       
-
-        % Rescale images for fitting speed
-        Zdata=imresize(Zdata,sc);X=imresize(X,sc);Y=imresize(Y,sc);
-
-        % Smooth data, extract peak
-        dSmooth=imgaussfilt(Zdata,0.5);   
-        N0=max(max(dSmooth));          
-
-        % Calculate guesses for center and size
-        Zx=sum(Zdata,1);Zy=sum(Zdata,2)';             % Get X and Y sum profiles
-        Nx=sum(Zx);Ny=sum(Zy);                % Get the total number of counts
-        Xc=mean(X(Zx>.9*max(X)));           % X center (use >90% SNR)
-        Yc=mean(Y(Zy>.9*max(Y)));           % Y center (use >90% SNR)
-        Xs=sqrt(sum((X-Xc).^2.*Zx)/Nx); % X standard deviation
-        Ys=sqrt(sum((Y-Yc).^2.*Zy)/Ny); % Y standard deviation       
-  
-        % Make a mesh grid for fitting
-        [xx,yy]=meshgrid(X,Y);
-
-        % Copy the data
-        Z2=Zdata;xx2=xx;yy2=yy;
-        
-        % Remove mask from fit
-        if hcMask.Value
-            ixon_mask_sc=imresize(ixon_mask,sc);
-            xx2(~ixon_mask_sc)=[];
-            yy2(~ixon_mask_sc)=[];
-            Z2(~ixon_mask_sc)=[];
-        end
-
-        % For ixon hopefully the background is 0
-        bg=0; 
-
-        % Create fit object
-        myfit=fittype('A*exp(-(xx-Xc).^2./(2*Xs^2)).*exp(-(yy-Yc).^2./(2*Ys^2))+nbg',...
-            'independent',{'xx','yy'},'coefficients',{'A','Xc','Xs','Yc','Ys','nbg'});
-        opt=fitoptions(myfit);
-        opt.StartPoint=[N0 Xc Xs Yc Ys bg];
-        opt.Lower=[N0/10 10 1 10 1 -1];
-        opt.Upper=[5*N0 max(X) range(X) max(Y) range(Y) N0];
-        opt.Weights=[];
-        
-
-        % Check that the upper and lower bounds make sense
-        badInds=opt.Upper<opt.Lower;
-        if sum(badInds)
-            warning(['Generated lower bounds for gaussian fit exceed the upper ' ...
-                'bounds for ' num2str(sum(badInds)) ' parameters. This ' ...
-                'may be caused by no atoms.']);
-            opt.Lower=[0 0 0 0 0 0];
-            opt.Upper=[];
-            opt.StartPoint=[100 mean(Dx) range(Dx)/10 mean(Dy) range(Dy)/10 ...
-                10];
-        end
-
-        % Display initial guess
-        str1=['(Xc0,Yc0)=(' num2str(round(Xc)) ',' num2str(round(Yc)) ');'];
-        str2=['(Xs0,Ys0)=(' num2str(round(Xs)) ',' num2str(round(Ys)) ')'];
-        fprintf([str1 str2 ';']);
-
-        % Perform the fit
-        fprintf(' fitting...');
-        t1=now;
-        [fout,gof,output]=fit([xx2(:) yy2(:)],Z2(:),myfit,opt);
-        t2=now;
-        disp([' done (' num2str(round((t2-t1)*24*60*60,1)) ' sec.).']);
-        GaussFit(k)={fout};
-    end
-    data.GaussFit=GaussFit;
-
-end
 
 %% FINISH
 data=updateImages(data);
