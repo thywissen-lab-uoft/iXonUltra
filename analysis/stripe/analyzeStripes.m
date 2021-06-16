82eb7d14422ce6a4137a91448b048d5c2a083559
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2021-06-16 16:52:18 -0400

    Update analyzeStripes.m

--- analysis/stripe/analyzeStripes.m
+++ analysis/stripe/analyzeStripes.m
@@ -10,12 +10,13 @@ ixondata=ixondata(inds);
 
 if nargin==2
    opts.theta=54;
-   opts.rotrange=[300 350];
-   opts.FitType='SmoothSquare';
-%    opts.FitType='Sine';
+   opts.rotrange=[220 300];
+   opts.FitType='Sine';
+%    opts.FitType='SmoothSquare';
    opts.LowThreshold=0.2;
+   opts.L0=140;
 end
-a
+
 %% Define the fit function
 
 switch opts.FitType
@@ -42,26 +43,21 @@ opt.MaxFunEvals=1000;
 opt.MaxFunEvals=1000;
 opt.TolFun=1E-7;
 
-figure
-tt=linspace(0,4*pi,1000);
-plot(tt,smoothSquare(tt,.5));
-
-%% Plot the figure
-
+%% Prepare the live figure
 
 hF_live=figure;
 hF_live.Position=[100 100 900 300];
 hF_live.Color='w';
-
 clf
+
 ax1=subplot(121);
-p1=plot(0,0,'-');
+pData=plot(0,0,'-');
 hold on
-p2=plot(0,0,'r-');
+pFit=plot(0,0,'r-');
+% pThresh=plot(0,0,'k--');
 
 xlabel('rotated vector');
 ylabel('summed counts');
-
 tt=text(.02,.98,'','units','normalized','fontsize',10,...
     'interpreter','latex','verticalalignment','top');
 
@@ -74,115 +70,124 @@ fRs={};
 axis equal tight
 
 Nsum=zeros(length(ixondata),1);
+
 for kk=1:length(ixondata)
+    
     % Grab the data and rotate
    Z=ixondata(kk).Z;
    Zrot=imrotate(Z,opts.theta,'bilinear','crop'); 
    
    % Select a sub ROI   
-   Zx=sum(Zrot(200:300,:),1);   
-%    Zx=sum(Zrot(:,:),1);      
+   Zx=sum(Zrot(opts.rotrange(1):opts.rotrange(2),:),1);   
    
-   Nsum(kk)=sum(sum(Zrot));
-   x=1:length(Zx);   
-   
+   x=1:length(Zx);      
 
+   % Construct some initial guessing
    xCguess=sum(Zx.*x)/sum(Zx);
    theta1=-4*pi;
    theta2=4*pi;
    Z0=max(Zx);
-   L0=180;
-   
+   L0=opts.L0;   
    
    % Make an initial Guess
-   switch opts.FitType
-       case 'SmoothSquare'     
-           opt.Start=[1.0*Z0 0.5 L0     0       xCguess    50      .1];     
-           opt.Lower=[0.5*Z0 0.0 0      theta1  min(x)     2 0 ]; 
-           opt.Upper=[1.5*Z0 1.5 L0+100 theta2  max(x)     1000 1];  
-       case 'Sine'
-          opt.Start=[max(Zx)     1 177 pi xCguess 70];     
-           opt.Lower=[0.5*max(Zx) .5 0 -pi 100 10]; 
-           opt.Upper=[max(Zx)*1.5 1.1 200 3*pi 600 200];  
-   end
+    switch opts.FitType
+        case 'SmoothSquare'     
+            opt.Start=[1.0*Z0 0.5 L0     0       xCguess    50      .1];     
+            opt.Lower=[0.5*Z0 0.0 L0-50      theta1  min(x)     2 0 ]; 
+            opt.Upper=[1.5*Z0 1.5 L0+50 theta2  max(x)     1000 1];  
+        case 'Sine'
+          opt.Start=[1.0*Z0 0.5 L0     0       xCguess    50      ];     
+            opt.Lower=[0.5*Z0 0.0 L0-50      theta1  min(x)     2]; 
+            opt.Upper=[1.5*Z0 1.5 L0+50 theta2  max(x)     1000];  
+    end
 
-   % Find data point to exclue
+    % Find data point to exclue
     ilow=(Zx<Zx/max(Zx*opts.LowThreshold));   
     opt.Exclude=ilow;
    
     % Fit the data
-   fRs{kk}=fit(x',Zx',myfit,opt);   
+    fR=fit(x',Zx',myfit,opt);  
+    
+    % Fit string label
+    str=['$\phi=' num2str(fR.phi/pi,2) '\pi$' newline ...
+       '$\lambda=' num2str(round(fR.L,1)) '~\mathrm{px}$' newline ...
+       '$\mathrm{depth}=' num2str(round(100*fR.B,1)) '\%$'];   
   
-   % Extract parameters
-  phis(kk)=mod(fRs{kk}.phi,2*pi);
-   Ls(kk)=fRs{kk}.L;
-   Bs(kk)=fRs{kk}.B;
-   
-      str=['$\phi=' num2str(phis(kk)/pi,2) '\pi$' newline ...
-       '$\lambda=' num2str(Ls(kk),0) '~\mathrm{px}$' newline ...
-       '$\mathrm{depth}=' num2str(round(100*Bs(kk),1)) '\%$'];
-   tt.String=str;
-   
    % Update the live plot
-    set(p1,'XData',x,'YData',Zx);
-    set(p2,'XData',x,'YData',feval(fRs{kk},x));
+    tt.String=str;
+    set(pData,'XData',x,'YData',Zx);
+    set(pFit,'XData',x,'YData',feval(fR,x));
+    
+%     set(pThresh,'XData',[x(1) x(end)],'YData',[1 1]*opts.LowThreshold);
+    
     set(ax1,'XLim',[1 length(x)],'YLim',[0 max(Zx)+100]);
     set(h1,'XData',1:size(Zrot,2),'YData',1:size(Zrot,1),'CData',Zrot);   
     set(ax2,'XLim',[1 size(Zrot,2)],'YLim',[1 size(Zrot,1)]);
-    drawnow;
-       
-
-   
-   waitforbuttonpress
-
-   
-       
+    drawnow;              
+    
+    % Append fit object
+    fRs{kk}=fR;
+    Nsum(kk)=sum(sum(Zrot));
 end
 
 
-%% Remove "bad"
+%% Process Fit Data
+phis=zeros(length(fRs),1);
+Ls=zeros(length(fRs),1);
+Bs=zeros(length(fRs),1);
+for kk=1:length(fRs)
+    ci=confint(fRs{kk});
+    disp(ci)
+    
+    phis(kk)=mod(fRs{kk}.phi,2*pi);
+    Ls(kk)=fRs{kk}.L;
+    Bs(kk)=fRs{kk}.B;    
+end
 
-i1=logical([Bs>0.3] .* [Nsum>6.5E6]');
-i2=~i1;
-% keyboard
+
 %% Plot the analysis
+[cface1,cedge1] = ixoncolororder(1);
+
+
 hf2=figure;
 hf2.Color='w';
 hf2.Position=[100 200 600 400];
 clf
 
-
+% Phase
 subplot(221);
-plot(xvals(i1),phis(i1)/pi,'o','linewidth',2)
+plot(xvals,phis/pi,'o','linewidth',2,'markerfacecolor',...
+    cface1,'markeredgecolor',cedge1)
 hold on
-plot(xvals(i2),phis(i2)/pi,'o','linewidth',2)
 ylabel('\phi (\pi)');
 xlabel(xVar);
 
+% Wavelength
 subplot(222)
-plot(xvals(i1),Ls(i1),'o','linewidth',2)
+plot(xvals,Ls,'o','linewidth',2,'markerfacecolor',...
+    cface1,'markeredgecolor',cedge1)
 hold on
-plot(xvals(i2),Ls(i2),'o','linewidth',2)
 ylabel('\lambda (px)');
 xlabel(xVar);
 
+% Total Counts
 subplot(223)
-plot(xvals(i1),Nsum(i1),'o','linewidth',2)
+plot(xvals,Nsum,'o','linewidth',2,'markerfacecolor',...
+    cface1,'markeredgecolor',cedge1)
 hold on
-plot(xvals(i2),Nsum(i2),'o','linewidth',2)
 ylabel('sum counts');
 xlabel(xVar);
 yL=get(gca,'YLim');
 ylim([0 yL(2)]);
 
+% Modulation Depth
 subplot(224)
-plot(xvals(i1),Bs(i1),'o','linewidth',2)
+plot(xvals,Bs,'o','linewidth',2,'markerfacecolor',...
+    cface1,'markeredgecolor',cedge1)
 hold on
-plot(xvals(i2),Bs(i2),'o','linewidth',2)
 ylabel('modulation depth');
 xlabel(xVar);
-% ylim([-1 1]*50+Lc);
-
+ylim([0 1]);
 
 
 end
