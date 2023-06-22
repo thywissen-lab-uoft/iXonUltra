33f2692894cf75c076883290b01fced27a48a1b6
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-06-22 13:09:06 -0400

    directory fucntion update

--- gui/ixon_gui.m
+++ gui/ixon_gui.m
@@ -3485,17 +3485,18 @@ end
 
 
 %% HELPER
-function s3=getDayDir
-    t=now;
-    
-    d=['X:\Data'];
+function s3=getDayDir(src)    
 
-    if ~exist(d,'dir')
+    % Default root directory
+    if nargin ~= 1;src=['X:\Data'];end
+
+    if ~exist(src,'dir')
         s3=pwd;
         return;
     end
+    
     s1=datestr(t,'yyyy');s2=datestr(t,'yyyy.mm');s3=datestr(t,'mm.dd');
-    s1=[d filesep s1];s2=[s1 filesep s2];s3=[s2 filesep s3];
+    s1=[src filesep s1];s2=[s1 filesep s2];s3=[s2 filesep s3];
 
     if ~exist(s1,'dir'); mkdir(s1); end
     if ~exist(s2,'dir'); mkdir(s2); end
