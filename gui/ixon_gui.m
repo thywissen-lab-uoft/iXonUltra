b71f7cc70b0e8684a4c102e201dd50c245400951
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-06-22 13:17:10 -0400

    Update ixon_gui.m

--- gui/ixon_gui.m
+++ gui/ixon_gui.m
@@ -3095,21 +3095,6 @@ function data=updateAnalysis(data)
 end
 
 %% OTHER HELPER FUNCTIONS
-    function imgs=grabRawImages
-        % How many images to grab
-        [ret,first,last] = GetNumberNewImages;    
-        numpix=512^2;
-        % Grab the data (number just sets buffer size)
-        [ret,D] = GetAcquiredData(last*512^2);    
-        imgs={};
-        imgmats=zeros(512,512,last);
-        for j = 1:last % break up into individual images
-            ii=double(D((1+(j-1)*numpix):(j*numpix)));
-            imgs{j} = reshape(ii,512,512);
-            imgmats(:,:,j)=imgs{j};
-        end 
-        imgs=imgmats;  
-    end
 
     function mydata=makeImgDataStruct(imgs)
         mydata=struct;
@@ -3269,7 +3254,23 @@ end
 
 
 %% Camera Functions
+    function imgs=grabRawImages
+        % How many images to grab
+        [ret,first,last] = GetNumberNewImages;    
+        numpix=512^2;
+        % Grab the data (number just sets buffer size)
+        [ret,D] = GetAcquiredData(last*512^2);    
+        imgs={};
+        imgmats=zeros(512,512,last);
+        for j = 1:last % break up into individual images
+            ii=double(D((1+(j-1)*numpix):(j*numpix)));
+            imgs{j} = reshape(ii,512,512);
+            imgmats(:,:,j)=imgs{j};
+        end 
+        imgs=imgmats;  
+    end
 
+    
 % Connect to the Andor iXon Ultra
 function out=connectCam
     out =0;
