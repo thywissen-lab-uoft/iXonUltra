3da99699ea3d85febc27b14ea71372985f8bd757
Cora <63414958+cjfujiwara@users.noreply.github.com>
2023-09-06 13:54:52 -0400

    using new guy

--- analysis/ixon_main.m
+++ analysis/ixon_main.m
@@ -1,10 +1,16 @@
 % ixonAnalysis.m
+%
 % Author : CF Fujiwara
+%
+% This script is the primary analysis file for the iXon MATLAB code. It
+% calls and plots all other analyses.
 
 % Display this filename
-disp(repmat('-',1,60));disp(repmat('-',1,60));    
+disp(repmat('-',1,60));    
+disp(repmat('-',1,60));    
 disp(['Calling ' mfilename '.m']);
-disp(repmat('-',1,60));disp(repmat('-',1,60));    
+disp(repmat('-',1,60));    
+disp(repmat('-',1,60));    
 
 % Add all subdirectories for this m file
 curpath = fileparts(mfilename('fullpath'));
@@ -31,32 +37,6 @@ lambda=770E-9;
 amu=1.660539E-27; 
 m=40*amu;
 
-%% Select image directory
-% Choose the directory where the images to analyze are stored
-disp([datestr(now,13) ' Choose an image analysis folder...']);
-dialog_title='Choose the root directory of the images';
-
-if ixon_getImageDir(datevec(now))
-    newdir=uigetdir(ixon_getImageDir(datevec(now)),dialog_title);
-    saveOpts = struct;
-    if isequal(newdir,0)
-        disp('Canceling.');    
-        return; 
-    else
-        imgdir = newdir;
-        ixon_imgdir = imgdir;
-        saveDir = [imgdir filesep 'figures'];
-        if ~exist(saveDir,'dir'); mkdir(saveDir);end   
-        saveOpts.saveDir=saveDir;
-        saveOpts.Quality = 'auto';
-        strs=strsplit(imgdir,filesep);
-        FigLabel=[strs{end-1} filesep strs{end}];
-    end
-else
-    disp('Canceling.');
-    return;
-end
-
 %% Analysis Variable
 % This section of code chooses the variable to plot against for aggregate
 % plots.  The chosen variable MUST match a variable provided in the params
@@ -64,18 +44,36 @@ end
 % display properties.
 
 % Choose what kind of variable to plot against (sequencer/camera)
-varType             = 'param'; % always select 'param' for now 
-ixon_autoXVar       = 1;      % Auto detect changing variable?
-ixon_autoUnit       = 1;      % Auto detect unit for variable?
-ixon_xVar           = 'qgm_raman_2photon_detuning'; % Variable Name
-ixon_overrideUnit   = 'V';    % If ixon_autoUnit=0, use this
-ixon_doSave         = 1;    % Save Analysis?
+varType='param'; % always select 'param' for now 
+
+% Should the analysis attempt to automatically find the xvariable?
+ixon_autoXVar = 1;
+
+% Should the analysis attempt to automatically find the unit?
+ixon_autoUnit=1;
+
+% The variable to plot against
+ixon_xVar='ExecutionDate';
+
+% If ixon_autoUnit=0, this will be used.
+ixon_overrideUnit='V';
+
+% Flag whether to save the output figures or not (code is faster if not
+% saving)
+ixon_doSave=1;
+
+% Define the output data
 outdata=struct;
 
 %% Analysis Options
 % Select what kinds of analyses you'd like to perform
-doRawImageHistogram=1;
+
+doRawImageHistogram=0;
+
+doDarkImageAnalysis = 1;
+
 ixon_doBoxCount=1;
+
 ixon_doGaussFit=0;
 
 % Fast Fourier Transform Analysis
@@ -90,29 +88,67 @@ doStripeAnalysis=0;
 
 ixon_doAnimate = 1;
 %% Image Processing Options
-
 % What do you do to the raw data?
 maskname=fullfile('ixon_mask.mat');
 ixon_mask=load(maskname);
 ixon_mask=ixon_mask.BW;
 
-img_opt = struct;
-img_opt.doSubtractBias      = 1;        % Subtract 200 count electronic offset
-img_opt.doScale             = 1;        % Scale up image? (good for single-site)
-img_opt.ScaleFactor         = 2;        % Amount to scale up by (x2 is good)
-img_opt.doRotate            = 1;        % Rotate image? (useful to align along lattices)
-img_opt.Theta               = 60.2077;  % Rotation amount (deg.)
-img_opt.doMask              = 0;        % Mask the data? (not used)
-img_opt.Mask                = ixon_mask;% Mask File 512x512
-img_opt.doGaussFilter       = 0;        % Filter the image? (bad for single-site)
-img_opt.GaussFilterRadius   = 1;        % Filter radius
-img_opt.doPSF               = 1;        % Deconolve with PSF
-img_opt.PSF                 = [1.3163 50 12]; % PSF parameters [sigma, N, Niter]
-img_opt.doFFT               = 1;        % Compute FFT?
-img_opt.doMaskIR            = 1;        % Mask long distance in FFT (useful)
-img_opt.IRMaskRadius        = 0.01;     % Mask radius in 1/px
-img_opt.doFFTFilter         = 1;        % Filter FFT?
-img_opt.FFTFilterRadius     = 1;        % FFT Filter radius (1/px)
+img_process = struct;
+img_process.doSubtractBias      = 1;
+img_process.doGaussFilter       = 0;
+img_process.GaussFilterRadius   = 1;
+img_process.doMask              = 0;
+img_process.Mask                = ixon_mask;
+img_process.doPSF               = 0;
+img_process.PSF                 = [1.3 50 5]; % [sigma, N, Niter]
+img_process.doFFT               = 1;
+img_process.doMaskIR            = 1;
+img_process.IRMaskRadius        = 0.01;
+img_process.doFFTFilter         = 1;
+img_process.FFTFilterRadius     = 1;
+
+
+%% Select image directory
+% % Choose the directory where the images to analyze are stored
+% disp([datestr(now,13) ' Choose an image analysis folder...']);
+% dialog_title='Choose the root dire ctory of the images';
+% ixon_imgdir=uigetdir(ixon_getImageDir(datevec(now)),dialog_title);
+% if isequal(ixon_imgdir,0)
+%     disp('Canceling.');
+%     return 
+% end
+% 
+% % Choose the directory where the images to analyze are stored
+disp([datestr(now,13) ' Choose an image analysis folder...']);
+dialog_title='Choose the root directory of the images';
+
+
+if ixon_getImageDir(datevec(now))
+    newdir=uigetdir(ixon_getImageDir(datevec(now)),dialog_title);
+    saveOpts = struct;
+
+    if isequal(newdir,0)
+        disp('Canceling.');    
+        return; 
+    else
+                imgdir = newdir;
+
+        ixon_imgdir = imgdir;
+        saveDir = [imgdir filesep 'figures'];
+
+        if ~exist(saveDir,'dir'); mkdir(saveDir);end    
+
+        saveOpts.saveDir=saveDir;
+        saveOpts.Quality = 'auto';
+
+        strs=strsplit(imgdir,filesep);
+        FigLabel=[strs{end-1} filesep strs{end}];
+    end
+else
+    disp('Canceling.');
+    return;
+end
+
 
 %% Load the data
 clear ixondata
@@ -120,41 +156,55 @@ disp(['Loading data from ' ixon_imgdir]);
 files=dir([ixon_imgdir filesep '*.mat']);
 files={files.name};
 
+
 for kk=1:length(files)
     str=fullfile(ixon_imgdir,files{kk});
     [a,b,c]=fileparts(str);      
     disp(['     (' num2str(kk) ')' files{kk}]);    
-    data=load(str);data=data.data;  
+    data=load(str);     
+    data=data.data;  
+
+    % Display image properties
     try
         disp(['     Image Name     : ' data.Name]);
         disp(['     Execution Time : ' datestr(data.Date)]);
         if ~ixon_autoXVar
             disp(['     ' ixon_xVar ' : ' num2str(data.Params.(ixon_xVar))]);
         end
+
         disp(' ');
     end    
+    
     data.Params.ExecutionDate = datenum(data.Params.ExecutionDate);
-    data.Params.ExecutionDateStr = datestr(data.Params.ExecutionDate); 
+data.Params.ExecutionDateStr = datestr(data.Params.ExecutionDate);    
+%     
+%     if isequal(ixon_xVar,'ExecutionDate')
+%         data.Params.(ixon_xVar)=datenum(data.Params.(ixon_xVar))*24*60*60;
+%     end    
     ixondata(kk)=data;    
 end
 disp(' ');
-%% Match Parameters and Flags
-% This makes sure that all data as has all the flags and params in each. 
-% This is usually not necessary.
+
 ixondata = ixon_matchParamsFlags(ixondata);
+
 %% X Variable and Units
 % If auto unit and variable are chosen, search through the parameters and
 % data to find which variable(s) are being changed.
+
+% Also, sort the data by that chosen variable.
 
 if ixon_autoXVar
     xVars = ixon_findXVars(ixondata);
     disp([' Found ' num2str(length(xVars)) ...
         ' valid variables that are changing to plot against.']);
-    disp(xVars);    
+    disp(xVars);
+    
     % Select the first variable that is changed
     ind = 1;    
-    ixon_xVar = xVars{ind};    
-    disp([' Setting ' ixon_xVar ' to be the x-variable']);    
+    ixon_xVar = xVars{ind};
+    
+    disp([' Setting ' ixon_xVar ' to be the x-variable']);
+    
     for kk=1:length(ixondata)
         disp([' (' num2str(kk) ') (' num2str(ixondata(kk).Params.(ixon_xVar)) ') ' ...
             ixondata(kk).Name]); 
@@ -197,24 +247,34 @@ switch varType
         error('uhh you chose the wrong thing to plot');
 end
 %% Assign Params to outdata
-
 outdata.Params=ixondata.Params;
 
+
+if ixon_doSave
+    Params =[ixondata.Params];
+    filename=fullfile(ixon_imgdir,'figures','Params.mat');
+    save(filename,'Params');
+end
 %% Analysis ROI
 % Analysis ROI is an Nx4 matrix of [X1 X2 Y1 Y2] which specifies a region
 % to analyze. Each new row in the matrix indicates a separate ROI to
 % perform analysis on.
 
-% Full ROI
-ixonROI = [1 1023 1 1023]; 
+% Full ROI 
+ixonROI = [1 1024 1 1024]; 
+
 [ixondata.ROI]=deal(ixonROI);
 
-%% Process the Raw Data
-ixondata = ixonProcessImages(ixondata,img_opt);
+%% Image Processing : Bias, Mask, and Filtering
+
+ixondata = processRawData(ixondata);
+
+
 
 %% Basic Raw Image Analysis
 
 if doRawImageHistogram   
+
     % Do basic analysis on raw counts
     ixondata=ixon_computeRawCounts(ixondata);
 
@@ -229,26 +289,66 @@ if doRawImageHistogram
     hist_opts.ImageNumber=1;    % Which image to histogram (overwritten)
     hist_opts.YScale='Log';     % Histogram y scale
     % hist_opts.YScale='Linear';
+
     for kk=1:size(ixondata(1).RawImages,3)
         hist_opts.ImageNumber=kk;
         hF_ixon_rawhist=ixon_showRawCountHistogram(ixondata,ixon_xVar,hist_opts);
         if ixon_doSave;ixon_saveFigure(ixondata,hF_ixon_rawhist,['ixon_raw_hist' num2str(kk)]);end
     end
+
     % Plot raw count total
     raw_opts=struct;   
-    raw_opts.xUnit=ixon_unit;    
+    raw_opts.xUnit=ixon_unit;
+    
     % Define the variable and units    
-    raw_opts.FitLinear=0;    
+    raw_opts.FitLinear=0;
+    
     hF_ixon_rawtotal=ixon_showRawCountTotal(ixondata,ixon_xVar,raw_opts);
+
     if ixon_doSave;ixon_saveFigure(ixondata,hF_ixon_rawtotal,['ixon_raw_counts']);end
+
 end
+
+%% Calculate FFT
+% 
+% fft_opts=struct;
+% fft_opts.doSmooth=1;
+% fft_opts.smoothRadius=1;
+% fft_opts.fft_N=2^11; % Can go higher for smoother data
+% 
+% fft_opts.maskIR=0;
+% fft_opts.maskUV=0;
+% fft_opts.LMax=50;
+% fft_opts.LMin=5;
+% 
+% 
+% if ixon_doFFT
+%     ixondata=ixon_computeFFT(ixondata,fft_opts);
+% end
+% 
+% % Apply makss to FFT Data
+% ixon_mask_IR=1;
+% ixon_mask_UV=0;
+% 
+% 
+% if fft_opts.maskIR
+%     ixondata=ixon_fft_maskIR(ixondata,fft_opts.LMax);    
+% end
+% 
+% if fft_opts.maskUV
+%     ixondata=ixon_fft_maskUV(ixondata,fft_opts.LMin);    
+% end
+
 %% ANALYSIS : FFT BOX COUNT
-if ixon_fft_doBoxCount && ixon_doFFT    
-    fft_boxOpts=struct;
-    fft_boxOpts.maskIR=fft_opts.maskIR;
-    fft_boxOpts.LMax=fft_opts.LMax;
-    fft_boxOpts.maskUV=fft_opts.maskUV;
-    fft_boxOpts.LMin=fft_opts.LMin;
+
+
+if ixon_fft_doBoxCount && ixon_doFFT
+    
+fft_boxOpts=struct;
+fft_boxOpts.maskIR=fft_opts.maskIR;
+fft_boxOpts.LMax=fft_opts.LMax;
+fft_boxOpts.maskUV=fft_opts.maskUV;
+fft_boxOpts.LMin=fft_opts.LMin;
     ixondata=ixon_fft_boxCount(ixondata,fft_boxOpts);
 end
 
@@ -257,16 +357,20 @@ end
 ixon_fft_boxPopts=struct;
 ixon_fft_boxPopts.xUnit=ixon_unit;
 
-if ixon_fft_doBoxCount  && ixon_doFFT 
+if ixon_fft_doBoxCount  && ixon_doFFT
+ 
     % Plot the second moments
     hF_ixon_size=ixon_fft_showBoxMoments(ixondata,ixon_xVar,ixon_fft_boxPopts);   
     if ixon_doSave;ixon_saveFigure(ixondata,hF_ixon_size,'ixon_fft_box_size');end     
+     
 end
 
 %% ANALYSIS : BOX COUNT
+
 if ixon_doBoxCount
     ixondata=ixon_boxCount(ixondata);
 end
+
 %% PLOTTING : BOX COUNT
 
 ixon_boxPopts=struct;
@@ -281,7 +385,8 @@ ixon_boxPopts.NumberExp2SumFit = 0;
 ixon_boxPopts.NumberLorentzianFit=0;
 
 ixon_boxPopts.CenterSineFit = 0;       % Fit sine fit to cloud center
-ixon_boxPopts.CenterDecaySineFit = 1;  % Fit decaying sine to cloud center
+ixon_boxPopts.CenterDecaySineFit = 0;  % Fit decaying sine to cloud center
+ixon_boxPopts.CenterGrowSineFit = 1;  % Fit decaying sine to cloud center
 ixon_boxPopts.CenterLinearFit = 0;     % Linear fit to cloud center
 
 if ixon_doBoxCount  
@@ -302,6 +407,28 @@ if ixon_doBoxCount
     if ixon_doSave;ixon_saveFigure(ixondata,hF_ixon_center,'ixon_box_centre');end 
 
     outdata.Ndatabox=Ndatabox;
+    outdata.Xcbox = Xc;
+    outdata.Ycbox = Yc;
+    
+    if ixon_doSave
+        BC = [ixondata.BoxCount];
+        P = [ixondata.Params];
+        BoxData_SourceFiles = {P.ExecutionDateStr};
+        BoxData_xVarName = ixon_xVar;
+        BoxData_xVarUnit = ixon_unit;
+        BoxData_xVar = [P.(ixon_xVar)];
+        BoxData_N = [BC.Ncounts];
+        BoxData_Xc = [BC.Xc];
+        BoxData_Yc = [BC.Yc];
+        BoxData_Xs = [BC.Xs];
+        BoxData_Ys = [BC.Ys];
+
+        filename=fullfile(ixon_imgdir,'figures','BoxData.mat');
+        save(filename,'BoxData_SourceFiles','BoxData_xVarName','BoxData_xVarUnit',...
+            'BoxData_xVar','BoxData_N','BoxData_Xc','BoxData_Yc',...
+            'BoxData_Xs','BoxData_Ys');
+    end
+    
 end
 
 %% ANALYSIS : 2D Gaussian
@@ -327,7 +454,7 @@ ixon_gauss_opts.NumberScale = 'linear';
 % ixon_gauss_opts.NumberScale = 'log'; 
 
 ixon_gauss_opts.CenterSineFit = 0;       % Fit sine fit to cloud center
-ixon_gauss_opts.CenterDecaySineFit = 0;  % Fit decaying sine to cloud center
+ixon_gauss_opts.CenterDecaySineFit = 1;  % Fit decaying sine to cloud center
 ixon_gauss_opts.CenterParabolaFit = 0;
 ixon_gauss_opts.CenterLinearFit = 0;     % Linear fit to cloud center
 
@@ -399,7 +526,7 @@ if ixon_doAnimate == 1 && ixon_doSave
     
     % Color limit for image
 %     ixon_animateOpts.CLim=[50 200];   % Color limits
-         ixon_animateOpts.CLim=[0 1500];   % Color limits
+         ixon_animateOpts.CLim=[0 300];   % Color limits
 
      ixon_animateOpts.CLim='auto';   % Automatically choose CLIM?
        ixon_animateOpts.CLim=[0 1000];   % Color limits
