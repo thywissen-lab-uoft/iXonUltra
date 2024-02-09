c6257dc00a910dc701c38dab47253d9a42288f64
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2024-02-09 01:34:21 -0500

    Update qgm_showStripeBin.m

--- analysis/dig_stripe/qgm_showStripeBin.m
+++ analysis/dig_stripe/qgm_showStripeBin.m
@@ -1,6 +1,6 @@
 function qgm_showStripeBin(qgmdata,xVar,opts)
     
-Nseps = 7;
+Nseps = 10;
 Ncenters = Nseps-1;
 
 n1 = 1:50;
@@ -41,16 +41,12 @@ Zb = zeros(50,50);
     
     % Text label for each string
     for kk=1:Ncenters
-        % str = ['score=' num2str(scores(kk),'%.2e')];
         tScores(kk) = text(1,1,'','horizontalalignment','left',...
             'verticalalignment','middle','fontsize',10,...
             'color',myc);
     end
     
     % Text summary of stripe fit
-    % str = ['$\lambda=' num2str(fout_s.L,'%.2f') ',' ...
-    %     '\phi=2\pi\cdot' num2str(round(mod(fout_s.phi,2*pi)/(2*pi),2),'%.2f') ',' ...
-    %     '\alpha = ' num2str(round(fout_s.B,2),'%.2f') '$'];
     tSummary = text(5,5,'','horizontalalignment','left',...
         'verticalalignment','bottom','fontsize',14,...
         'color',myc,'interpreter','latex','backgroundcolor',[0 0 0],'Margin',1,...
@@ -60,58 +56,51 @@ Zb = zeros(50,50);
     pStar = plot(1,1,'pentagram','markersize',12,...
         'markerfacecolor',myc,'markeredgecolor',myc*.8);
     
-    % %% Vertical Axis
-    % ax2=subplot(5,5,[5 10 15 20]);
-    % cla
-    % 
-    % % Data Plot
-    % pZs = plot(Zs,ns,'k-','linewidth',1,'color','k');
-    % hold on
-    % nsFit = linspace(min(ns),max(ns),1e3);
-    % 
-    % % Fit Plot
-    % pZsF = plot(feval(fout_s,nsFit),nsFit,'-','color',co(1,:),'linewidth',2);
-    % set(gca,'fontsize',12,'YAxisLocation','right','YColor',co(1,:),'fontname','times',...
-    %     'Xaxislocation','top')
-    % ylabel('$n_2$ (site)','interpreter','latex');
-    % ylim([min(ns) max(ns)])
-    % plot(stripe_envelope(nsFit),nsFit,'--','color',co(1,:),'linewidth',1)
-    % drawnow;
-    % 
-    % % Separations
-    % for kk=1:Nseps
-    %     pSeps2(kk) = plot(get(gca,'XLim'),[1 1]*seps(kk),'--','color',myc);
-    % end
-    % grid on
-    % strR = ['$R^2 = ' num2str(gof_t.rsquare,'%.3f') '$'];
-    % tRs = text(4,2,strR,'fontsize',10,'interpreter','latex','verticalalignment','bottom',...
-    %     'units','pixels');
+    %% Vertical Axis
+    ax2=subplot(5,5,[5 10 15 20]);
+    cla
+    
+    % Data Plot
+    pZs = plot(1,1,'k-','linewidth',1,'color','k');
+    hold on
+    pZsF = plot(1,1,'-','color',co(1,:),'linewidth',2);
+    set(gca,'fontsize',12,'YAxisLocation','right','YColor',co(1,:),'fontname','times',...
+        'Xaxislocation','top')
+    ylabel('$n_2$ (site)','interpreter','latex');
+    ylim([0 1])
+    
+%     pSEnv = plot(1,1,'--','color',co(1,:),'linewidth',1);
+%     drawnow;
+%     
+    % Separations
+    for kk=1:Nseps
+        pSeps2(kk) = plot(1,1,'--','color',myc);
+    end
+    grid on
+    tRs = text(4,2,'','fontsize',10,'interpreter','latex','verticalalignment','bottom',...
+        'units','pixels');
+    
+    
     % %% Horizontal Axis
-    % ax3=subplot(5,5,[21 22 23 24]);
-    % plot(nt,Zt,'k-','linewidth',1);
-    % hold on
-    % ntFit = linspace(min(nt),max(nt),1e3);
-    % plot(ntFit,feval(fout_t,ntFit),'-','color',co(2,:),'linewidth',2);
-    % set(gca,'ydir','normal','fontsize',12,'Xcolor',co(2,:),'fontname','times')
-    % xlabel('$n_1$ (site)','interpreter','latex');
-    % xlim([min(nt) max(nt)])
-    % grid on
-    % strR = ['$R^2 = ' num2str(gof_s.rsquare,'%.3f') '$'];
-    % text(4,2,strR,'fontsize',10,'interpreter','latex','verticalalignment','bottom',...
-    %     'units','pixels')
-    % 
-    % linkaxes([ax1 ax2],'y');
-    % linkaxes([ax1 ax3],'x');
-    % 
+    ax3=subplot(5,5,[21 22 23 24]);
+    pZt = plot(1,1,'k-','linewidth',1);
+    hold on
+    pZtF = plot(1,1,'-','color',co(2,:),'linewidth',2);
+    set(gca,'ydir','normal','fontsize',12,'Xcolor',co(2,:),'fontname','times')
+    xlabel('$n_1$ (site)','interpreter','latex');
+    xlim([0 1])
+    grid on
+    tRt = text(4,2,'','fontsize',10,'interpreter','latex','verticalalignment','bottom',...
+        'units','pixels');
+
     % %% Score Axis
-    % ax4 = subplot(5,5,25);
-    % pScore = plot(centers,scores,'ko','markerfacecolor',[.5 .5 .5]);
-    % xlabel('fringe position (site)');
-    % ylabel('score');
-    % set(gca,'fontsize',8);
-    % yL =get(gca,'YLim');
-    % ylim([0 yL(2)]);
-    % xlim([min(n1) max(n1)])
+    ax4 = subplot(5,5,25);
+    pScore = plot(1,1,'ko','markerfacecolor',[.5 .5 .5]);
+    xlabel('fringe position (site)');
+    ylabel('score');
+    set(gca,'fontsize',8);
+    ylim([0 1]);
+    xlim([0 1])
 
     %% main Loop
 
@@ -119,11 +108,15 @@ Zb = zeros(50,50);
         n1 = [qgmdata(nn).LatticeBin.n1];
         n2 = [qgmdata(nn).LatticeBin.n2];
         Zb = [qgmdata(nn).LatticeBin.Zbin];
+        Zb2 = Zb;
+        Zb2(Zb<opts.ColorThreshold(1)) = 0;
+        Zb2(isnan(Zb)) = 0;
+        
         BS = [qgmdata(nn).BinStripe];
 
         % Update Main Image
         set(hImg,'CData',Zb,'XData',n1,'YData',n2);
-
+        set(ax1,'XLim',[min(n1) max(n1)],'YLim',[min(n2) max(n2)]);
     
         % Update Separation Bars
         for kk = 1:length(pSeps)
@@ -138,20 +131,43 @@ Zb = zeros(50,50);
             tScores(kk).Visible='off';
         end
         for kk=1:length([BS.Centers])
-            str = ['score=' num2str(scores(kk),'%.2e')];
+            str = ['score=' num2str(BS.Scores(kk),'%.2e')];
             set(tScores(kk),'String',str,'Position',[min(n1)+6 BS.Centers(kk)],'Visible','on')
         end
         set(pStar,'XData',min(n1)+1,'YData',BS.FocusCenter);
 
-
         % Update Summary 
-        str = ['$\lambda=' num2str(BS.fitStripe.L,'%.2f') ',' ...
-            '\phi=2\pi\cdot' num2str(round(mod(BS.fitStripe.phi,2*pi)/(2*pi),2),'%.2f') ',' ...
-            '\alpha = ' num2str(round(BS.fitStripe.B,2),'%.2f') '$'];
+        str = ['$\lambda=' num2str(BS.FitStripe.L,'%.2f') ',' ...
+            '\phi=2\pi\cdot' num2str(mod(BS.FitStripe.phi,2*pi)/(2*pi),'%.2f') ',' ...
+            '\alpha = ' num2str(BS.FitStripe.B,'%.2f') '$'];
         set(tSummary,'String',str);
+        
+        set(pZs,'XData',sum(Zb2,2)','YData',n2);
+        set(pZsF,'XData',feval(BS.FitStripe,n2)','YData',n2);
+        set(ax2,'YLim',[min(n2) max(n2)]);
+        
+        % Update Separation Bars
+        for kk = 1:length(pSeps2)
+            pSeps2(kk).Visible='off';
+        end
+        for kk=1:length([BS.Separations])
+            set(pSeps2(kk),'XData',get(ax2,'XLim'),'YData',[1 1]*BS.Separations(kk),'Visible','on');
+        end
+        set(tRs,'String',['$R^2 = ' num2str(BS.RSquareStripe,'%.3f') '$']);
+
+        set(pZt,'YData',sum(Zb2,1)','XData',n1);
+        set(pZtF,'YData',feval(BS.FitTransverse,n1)','XData',n1);
+        set(ax3,'XLim',[min(n1) max(n1)]);
+        set(tRt,'String',['$R^2 = ' num2str(BS.RSquareTransverse,'%.3f') '$']);
 
 
-
+        set(pScore,'XData',[BS.Centers],'YData',[BS.Scores]);
+        try
+        set(ax4,'XLim',[min(n1) max(n1)],'YLim',[0 max([BS.Scores])*1.2]);
+        end
+        drawnow;
+        
+        
     end
 end
 
