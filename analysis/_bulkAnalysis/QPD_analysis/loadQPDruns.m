b0e8ff870d4e4c4493ce2eeeaee3ebc73ef2bcdb
robynlearn <robyn.learn@mail.utoronto.ca>
2024-02-14 13:47:35 -0500

    QPD analysis stuff
    Added new functions to pull and analyze the QPD traces during a specific data run

---
+++ analysis/_bulkAnalysis/QPD_analysis/loadQPDruns.m
@@ -0,0 +1,84 @@
+function [ixondata,qpd_out] = loadQPDruns(ixondata,qpdfiles,opts)
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Analyzes list of QPD filenames 
+% Outputs average powers, fit parameters, raw channels with time shifted to
+% start of modulation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Initialize options
+
+if nargin~=2
+    opts.doPlot = 1;
+    opts.doSave = 0;
+end
+
+%% Close plots
+
+if ishandle(101)
+    close(101)
+end
+if ishandle(201)
+    close(201)
+end
+if ishandle(301)
+    close(301)
+end
+
+%% Analyze the list of QPD files
+
+% Initialize data struct
+% qpd_out = struct;
+
+for nn=1:length(qpdfiles)
+
+    % Analyze single QPD file
+    qpd_data(nn) = loadQPD(qpdfiles(nn));
+
+    ixondata(nn).qpd_data = qpd_data(nn);
+
+end
+
+%% Assign analyzed data to output
+
+qpd_out.qpd_data = qpd_data;
+
+%% Compute average parameters for all runs
+
+% Take average and standard deviation of fit values during first 50ms
+    % of modulation
+
+qpd_out.mod_params.X1.A     = mean([qpd_data.modfit_X1_A]);
+qpd_out.mod_params.X1.A_err = std([qpd_data.modfit_X1_A]);
+qpd_out.mod_params.X1.B     = mean([qpd_data.modfit_X1_B]);
+qpd_out.mod_params.X1.B_err = std([qpd_data.modfit_X1_B]);
+qpd_out.mod_params.X1.C     = mean([qpd_data.modfit_X1_C]);
+qpd_out.mod_params.X1.C_err = std([qpd_data.modfit_X1_C]);
+qpd_out.mod_params.X1.T     = mean([qpd_data.modfit_X1_T]);
+qpd_out.mod_params.X1.T_err = std([qpd_data.modfit_X1_T]);
+
+qpd_out.mod_params.X2.A     = mean([qpd_data.modfit_X2_A]);
+qpd_out.mod_params.X2.A_err = std([qpd_data.modfit_X2_A]);
+qpd_out.mod_params.X2.B     = mean([qpd_data.modfit_X2_B]);
+qpd_out.mod_params.X2.B_err = std([qpd_data.modfit_X2_B]);
+qpd_out.mod_params.X2.C     = mean([qpd_data.modfit_X2_C]);
+qpd_out.mod_params.X2.C_err = std([qpd_data.modfit_X2_C]);
+qpd_out.mod_params.X2.T     = mean([qpd_data.modfit_X2_T]);
+qpd_out.mod_params.X2.T_err = std([qpd_data.modfit_X2_T]);
+
+
+% Take average and standard deviation of average powers during -350ms to 50ms
+
+qpd_out.powers.ODT1      = mean([qpd_data.ODT1_ave]);
+qpd_out.powers.ODT1_err  = std([qpd_data.ODT1_ave]);
+qpd_out.powers.ODT2      = mean([qpd_data.ODT2_ave]);
+qpd_out.powers.ODT2_err  = std([qpd_data.ODT2_ave]);
+qpd_out.powers.XLatt     = mean([qpd_data.XLatt_ave]);
+qpd_out.powers.XLatt_err = std([qpd_data.XLatt_ave]);
+qpd_out.powers.YLatt     = mean([qpd_data.YLatt_ave]);
+qpd_out.powers.YLatt_err = std([qpd_data.YLatt_ave]);
+qpd_out.powers.ZLatt     = mean([qpd_data.ZLatt_ave]);
+qpd_out.powers.ZLatt_err = std([qpd_data.ZLatt_ave]);
+
+
+end
