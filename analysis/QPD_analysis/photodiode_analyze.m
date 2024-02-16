94d5bdea281cecbdfb6952cfcb886fa399b9404c
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2024-02-16 12:36:31 -0500

    shifting more

--- analysis/QPD_analysis/photodiode_analyze.m
+++ analysis/QPD_analysis/photodiode_analyze.m
@@ -7,109 +7,109 @@ function qpd_out = photodiode_analyze(qpd_filename)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% Calling this loadQPD.m is a misnomer
-%% Initialize data struct and load QPD data
+for nn=1:length(qpd_filename)
+    %% Initialize data struct and load QPD data
 
-qpd_out = struct;
-qpd_single = load(qpd_filename);
+    qpd_single = load(qpd_filename);
 
-%% Shift the time such that modulation start at t=0ms
+    %% Shift the time such that modulation start at t=0ms
 
-% Index of modulation start and convert to ms
-mod_start = max(find(diff(qpd_single.data(:,10))==1));
+    % Index of modulation start and convert to ms
+    mod_start = max(find(diff(qpd_single.data(:,10))==1));
 
-% Shift time by modulation start time
-t = (qpd_single.t - qpd_single.t(mod_start))*1e3;
+    % Shift time by modulation start time
+    t = (qpd_single.t - qpd_single.t(mod_start))*1e3;
 
-%% Save all of the traces with the correct shifted time
+    %% Save all of the traces with the correct shifted time
 
-qpd_out.t       = t;
-qpd_out.X1      = qpd_single.data(:,1);
-qpd_out.Y1      = qpd_single.data(:,2);
-qpd_out.SUM1    = qpd_single.data(:,3);
-qpd_out.X2      = qpd_single.data(:,4);
-qpd_out.Y2      = qpd_single.data(:,5);
-qpd_out.SUM2    = qpd_single.data(:,6);
-qpd_out.XLATT   = qpd_single.data(:,7);
-qpd_out.YLATT   = qpd_single.data(:,8);
-qpd_out.ZLATT   = qpd_single.data(:,9);
-qpd_out.TRIG    = qpd_single.data(:,10);
+    qpd_out(nn).t       = t;
+    qpd_out(nn).X1      = qpd_single.data(:,1);
+    qpd_out(nn).Y1      = qpd_single.data(:,2);
+    qpd_out(nn).SUM1    = qpd_single.data(:,3);
+    qpd_out(nn).X2      = qpd_single.data(:,4);
+    qpd_out(nn).Y2      = qpd_single.data(:,5);
+    qpd_out(nn).SUM2    = qpd_single.data(:,6);
+    qpd_out(nn).XLATT   = qpd_single.data(:,7);
+    qpd_out(nn).YLATT   = qpd_single.data(:,8);
+    qpd_out(nn).ZLATT   = qpd_single.data(:,9);
+    qpd_out(nn).TRIG    = qpd_single.data(:,10);
 
-%% Average the power over the the 200ms before the ramp --> first 50ms of mod.
+    %% Average the power over the the 200ms before the ramp --> first 50ms of mod.
 
-% Select time to average over - right now choosing 200 ms before ramp
-% time, and first 50 ms of modulation
-% modulation 
+    % Select time to average over - right now choosing 200 ms before ramp
+    % time, and first 50 ms of modulation
+    % modulation 
 
-start_ave = find(round(t,1)==-350);
-end_ave   = find(round(t,1)==50);
+    start_ave = find(round(t,1)==-350);
+    end_ave   = find(round(t,1)==50);
 
-qpd_out.t_ave = t(start_ave:end_ave);
+    qpd_out(nn).t_ave = t(start_ave:end_ave);
 
-% Average the powers over the averaging window
+    % Average the powers over the averaging window
 
-qpd_out.ODT1_ave = mean(qpd_single.data(start_ave:end_ave,3));
-qpd_out.ODT1_std = std(qpd_single.data(start_ave:end_ave,3));
+    qpd_out(nn).ODT1_ave = mean(qpd_single.data(start_ave:end_ave,3));
+    qpd_out(nn).ODT1_std = std(qpd_single.data(start_ave:end_ave,3));
 
-qpd_out.ODT2_ave = mean(qpd_single.data(start_ave:end_ave,6));
-qpd_out.ODT2_std = std(qpd_single.data(start_ave:end_ave,6));
+    qpd_out(nn).ODT2_ave = mean(qpd_single.data(start_ave:end_ave,6));
+    qpd_out(nn).ODT2_std = std(qpd_single.data(start_ave:end_ave,6));
 
-qpd_out.XLatt_ave = mean(qpd_single.data(start_ave:end_ave,7));
-qpd_out.XLatt_std = std(qpd_single.data(start_ave:end_ave,7));
+    qpd_out(nn).XLatt_ave = mean(qpd_single.data(start_ave:end_ave,7));
+    qpd_out(nn).XLatt_std = std(qpd_single.data(start_ave:end_ave,7));
 
-qpd_out.YLatt_ave = mean(qpd_single.data(start_ave:end_ave,8));
-qpd_out.YLatt_std = std(qpd_single.data(start_ave:end_ave,8));
+    qpd_out(nn).YLatt_ave = mean(qpd_single.data(start_ave:end_ave,8));
+    qpd_out(nn).YLatt_std = std(qpd_single.data(start_ave:end_ave,8));
 
-qpd_out.ZLatt_ave = mean(qpd_single.data(start_ave:end_ave,9));
-qpd_out.ZLatt_std = std(qpd_single.data(start_ave:end_ave,9));
+    qpd_out(nn).ZLatt_ave = mean(qpd_single.data(start_ave:end_ave,9));
+    qpd_out(nn).ZLatt_std = std(qpd_single.data(start_ave:end_ave,9));
 
-%% Fit the first 50 ms of modulation
+    %% Fit the first 50 ms of modulation
 
-% Select the first 50 ms of modulation
-mod50 = find(round(t,1)==50);
-t_fitmod = t(mod_start:mod50);
+    % Select the first 50 ms of modulation
+    mod50 = find(round(t,1)==50);
+    t_fitmod = t(mod_start:mod50);
 
-% Normalize the data by the sum
-X1_fitmod = qpd_single.data(mod_start:mod50,1)./qpd_single.data(mod_start:mod50,3);
-X2_fitmod = qpd_single.data(mod_start:mod50,4)./qpd_single.data(mod_start:mod50,6);
+    % Normalize the data by the sum
+    X1_fitmod = qpd_single.data(mod_start:mod50,1)./qpd_single.data(mod_start:mod50,3);
+    X2_fitmod = qpd_single.data(mod_start:mod50,4)./qpd_single.data(mod_start:mod50,6);
 
-% Basic sin func to fit
-myfunc = @(A,B,C,T,t) A*sin(2*pi*t/T + B) + C;    
+    % Basic sin func to fit
+    myfunc = @(A,B,C,T,t) A*sin(2*pi*t/T + B) + C;    
 
-myfit = fittype(@(A,B,C,T,t) myfunc(A,B,C,T,t),'independent',{'t'},...
-    'coefficients',{'A','B','C','T'});
+    myfit = fittype(@(A,B,C,T,t) myfunc(A,B,C,T,t),'independent',{'t'},...
+        'coefficients',{'A','B','C','T'});
 
-Agp = (max(X1_fitmod)-min(X1_fitmod))/2;
-Cgp = mean(X1_fitmod);
-Tgp = 1/(0.06);
-Bgp =  mod(2*pi*(150)/Tgp,2*pi);
-opt = fitoptions(myfit);
-opt.StartPoint = [Agp Bgp Cgp Tgp];
-opt.Lower  = [0 -10*pi Cgp-20 0];
-fout_X1 = fit(t_fitmod',X1_fitmod,myfit,opt);
+    Agp = (max(X1_fitmod)-min(X1_fitmod))/2;
+    Cgp = mean(X1_fitmod);
+    Tgp = 1/(0.06);
+    Bgp =  mod(2*pi*(150)/Tgp,2*pi);
+    opt = fitoptions(myfit);
+    opt.StartPoint = [Agp Bgp Cgp Tgp];
+    opt.Lower  = [0 -10*pi Cgp-20 0];
+    fout_X1 = fit(t_fitmod',X1_fitmod,myfit,opt);
 
-Agp = (max(X2_fitmod)-min(X2_fitmod))/2;
-Cgp = mean(X2_fitmod);
-Bgp =  mod(2*pi*(150)/Tgp,2*pi);
-opt = fitoptions(myfit);
-opt.StartPoint = [Agp Bgp Cgp Tgp];
-opt.Lower  = [0 -10*pi Cgp-20 0];
-fout_X2 = fit(t_fitmod',X2_fitmod,myfit,opt);
+    Agp = (max(X2_fitmod)-min(X2_fitmod))/2;
+    Cgp = mean(X2_fitmod);
+    Bgp =  mod(2*pi*(150)/Tgp,2*pi);
+    opt = fitoptions(myfit);
+    opt.StartPoint = [Agp Bgp Cgp Tgp];
+    opt.Lower  = [0 -10*pi Cgp-20 0];
+    fout_X2 = fit(t_fitmod',X2_fitmod,myfit,opt);
 
-qpd_out.modfit_t      = t_fitmod;
-qpd_out.modfit_normX1data = X1_fitmod;
-qpd_out.modfit_normX2data = X2_fitmod;
+    qpd_out(nn).modfit_t      = t_fitmod;
+    qpd_out(nn).modfit_normX1data = X1_fitmod;
+    qpd_out(nn).modfit_normX2data = X2_fitmod;
 
-qpd_out.modfit_X1     = fout_X1;
-qpd_out.modfit_X2     = fout_X2;
+    qpd_out(nn).modfit_X1     = fout_X1;
+    qpd_out(nn).modfit_X2     = fout_X2;
 
-qpd_out.modfit_X1_A   = fout_X1.A;
-qpd_out.modfit_X2_A   = fout_X2.A;
-qpd_out.modfit_X1_B   = mod(fout_X1.B,2*pi);
-qpd_out.modfit_X2_B   = mod(fout_X2.B,2*pi);
-qpd_out.modfit_X1_C   = fout_X1.C;
-qpd_out.modfit_X2_C   = fout_X2.C;
-qpd_out.modfit_X1_T   = fout_X1.T;
-qpd_out.modfit_X2_T   = fout_X2.T;
+    qpd_out(nn).modfit_X1_A   = fout_X1.A;
+    qpd_out(nn).modfit_X2_A   = fout_X2.A;
+    qpd_out(nn).modfit_X1_B   = mod(fout_X1.B,2*pi);
+    qpd_out(nn).modfit_X2_B   = mod(fout_X2.B,2*pi);
+    qpd_out(nn).modfit_X1_C   = fout_X1.C;
+    qpd_out(nn).modfit_X2_C   = fout_X2.C;
+    qpd_out(nn).modfit_X1_T   = fout_X1.T;
+    qpd_out(nn).modfit_X2_T   = fout_X2.T;
+end
 
 end
--- analysis/QPD_analysis/photodiode_collect_analysis.m
+++ analysis/QPD_analysis/photodiode_collect_analysis.m
@@ -1,4 +1,4 @@
-function [pd_summary,pd_data] = photodiode_collect_analysis(qpdfiles,opts)
+function [hF,pd_summary] = photodiode_collect_analysis(pd_data,opts)
 
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Analyzes list of QPD filenames 
@@ -13,55 +13,264 @@ if nargin~=2
     opts.doSave = 0;
 end
 
-% - should plotting be separated from analysis?
-
-%% Analyze the list of QPD files
-
-% Initialize data struct
-% qpd_out = struct;
-
-for nn=1:length(qpdfiles)
-    pd_data(nn) = photodiode_analyze(qpdfiles(nn));
-end
-
 %% Assign analyzed data to output
 
-pd_out.qpd_data = pd_data;
+pd_summary.qpd_data = pd_data;
 
 %% Compute average parameters for all runs
-
 % Take average and standard deviation of fit values during first 50ms
     % of modulation
+pd_summary.mod_params.X1.A     = mean([pd_data.modfit_X1_A]);
+pd_summary.mod_params.X1.A_err = std([pd_data.modfit_X1_A]);
+pd_summary.mod_params.X1.B     = mean([pd_data.modfit_X1_B]);
+pd_summary.mod_params.X1.B_err = std([pd_data.modfit_X1_B]);
+pd_summary.mod_params.X1.C     = mean([pd_data.modfit_X1_C]);
+pd_summary.mod_params.X1.C_err = std([pd_data.modfit_X1_C]);
+pd_summary.mod_params.X1.T     = mean([pd_data.modfit_X1_T]);
+pd_summary.mod_params.X1.T_err = std([pd_data.modfit_X1_T]);
 
-pd_out.mod_params.X1.A     = mean([pd_data.modfit_X1_A]);
-pd_out.mod_params.X1.A_err = std([pd_data.modfit_X1_A]);
-pd_out.mod_params.X1.B     = mean([pd_data.modfit_X1_B]);
-pd_out.mod_params.X1.B_err = std([pd_data.modfit_X1_B]);
-pd_out.mod_params.X1.C     = mean([pd_data.modfit_X1_C]);
-pd_out.mod_params.X1.C_err = std([pd_data.modfit_X1_C]);
-pd_out.mod_params.X1.T     = mean([pd_data.modfit_X1_T]);
-pd_out.mod_params.X1.T_err = std([pd_data.modfit_X1_T]);
-
-pd_out.mod_params.X2.A     = mean([pd_data.modfit_X2_A]);
-pd_out.mod_params.X2.A_err = std([pd_data.modfit_X2_A]);
-pd_out.mod_params.X2.B     = mean([pd_data.modfit_X2_B]);
-pd_out.mod_params.X2.B_err = std([pd_data.modfit_X2_B]);
-pd_out.mod_params.X2.C     = mean([pd_data.modfit_X2_C]);
-pd_out.mod_params.X2.C_err = std([pd_data.modfit_X2_C]);
-pd_out.mod_params.X2.T     = mean([pd_data.modfit_X2_T]);
-pd_out.mod_params.X2.T_err = std([pd_data.modfit_X2_T]);
+pd_summary.mod_params.X2.A     = mean([pd_data.modfit_X2_A]);
+pd_summary.mod_params.X2.A_err = std([pd_data.modfit_X2_A]);
+pd_summary.mod_params.X2.B     = mean([pd_data.modfit_X2_B]);
+pd_summary.mod_params.X2.B_err = std([pd_data.modfit_X2_B]);
+pd_summary.mod_params.X2.C     = mean([pd_data.modfit_X2_C]);
+pd_summary.mod_params.X2.C_err = std([pd_data.modfit_X2_C]);
+pd_summary.mod_params.X2.T     = mean([pd_data.modfit_X2_T]);
+pd_summary.mod_params.X2.T_err = std([pd_data.modfit_X2_T]);
 
 % Take average and standard deviation of average powers during -350ms to 50ms
-pd_out.powers.ODT1      = mean([pd_data.ODT1_ave]);
-pd_out.powers.ODT1_err  = std([pd_data.ODT1_ave]);
-pd_out.powers.ODT2      = mean([pd_data.ODT2_ave]);
-pd_out.powers.ODT2_err  = std([pd_data.ODT2_ave]);
-pd_out.powers.XLatt     = mean([pd_data.XLatt_ave]);
-pd_out.powers.XLatt_err = std([pd_data.XLatt_ave]);
-pd_out.powers.YLatt     = mean([pd_data.YLatt_ave]);
-pd_out.powers.YLatt_err = std([pd_data.YLatt_ave]);
-pd_out.powers.ZLatt     = mean([pd_data.ZLatt_ave]);
-pd_out.powers.ZLatt_err = std([pd_data.ZLatt_ave]);
+pd_summary.powers.ODT1      = mean([pd_data.ODT1_ave]);
+pd_summary.powers.ODT1_err  = std([pd_data.ODT1_ave]);
+pd_summary.powers.ODT2      = mean([pd_data.ODT2_ave]);
+pd_summary.powers.ODT2_err  = std([pd_data.ODT2_ave]);
+pd_summary.powers.XLatt     = mean([pd_data.XLatt_ave]);
+pd_summary.powers.XLatt_err = std([pd_data.XLatt_ave]);
+pd_summary.powers.YLatt     = mean([pd_data.YLatt_ave]);
+pd_summary.powers.YLatt_err = std([pd_data.YLatt_ave]);
+pd_summary.powers.ZLatt     = mean([pd_data.ZLatt_ave]);
+pd_summary.powers.ZLatt_err = std([pd_data.ZLatt_ave]);
+
+%% 
+
+if opts.doPlot
+    hF = figure;
+
+
+    for nn=1:length(qpdfiles)
+
+   
+        qpd_data(nn) = ixondata(nn).qpd_data;
+
+        %%%%%%%%%%%%% Plot individual modulation traces %%%%%%%%%%%%%%%
+
+        tt=linspace(min(qpd_data(nn).modfit_t),max(qpd_data(nn).modfit_t),1e3);
+    
+        f=figure(9001);
+        f.Position = [100 100 540 400]; 
+        clf;
+    
+        % Plot X1
+        subplot(211)
+        plot(qpd_data(nn).modfit_t,qpd_data(nn).modfit_normX1data)
+        hold on;
+        plot(tt,feval(qpd_data(nn).modfit_X1,tt),'-')
+        hold off;
+    
+        xlim([0 50])
+        xlabel('Time (ms)')
+        ylabel('Normalized X1')
+    
+        % Plot X2
+        subplot(212)
+        plot(qpd_data(nn).modfit_t,qpd_data(nn).modfit_normX2data)
+        hold on;
+        plot(tt,feval(qpd_data(nn).modfit_X2,tt))
+        hold off;
+    
+        xlim([0 50])
+        xlabel('Time (ms)')
+        ylabel('Normalized X2')
+    
+
+        %%%%%%%%%%%% Plot summary of modulation traces %%%%%%%%%%%%%%
+
+        % Select the data during the modulation, from ramp up to end
+%         ramp_start = find(round(qpd_data(nn).t,1)==-150);
+        ramp_start = find(round(qpd_data(nn).t,1)==-ixondata(nn).Params.conductivity_mod_ramp_time);
+
+        % Find end of mod from lattice ramp up BUGGY RL FIX
+        % (approx. as max slope of lattice ramp)
+%         mod_end   = find(diff(qpd_data(nn).XLATT)==max(diff(qpd_data(nn).XLATT)));
+        mod_end   = max(find(diff(qpd_data(nn).TRIG)==-1));
+
+        % Add 50 ms buffer time to each end 
+        buffer_t = 250;
+    
+        fmsum=figure(201);
+        fmsum.Position = [600 100 800 600];
+        plotColor = jet(length(qpdfiles));
+
+        ax1=subplot(4,3,1);
+        plot(qpd_data(nn).t,qpd_data(nn).X1./qpd_data(nn).SUM1,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 1 X (V/V)')
+    
+        ax2=subplot(4,3,4);
+        plot(qpd_data(nn).t,qpd_data(nn).Y1./qpd_data(nn).SUM1,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 1 Y (V/V)')
+    
+        ax3=subplot(4,3,7);
+        plot(qpd_data(nn).t,qpd_data(nn).X2./qpd_data(nn).SUM2,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 2 X (V/V)')
+    
+        ax4=subplot(4,3,10);
+        plot(qpd_data(nn).t,qpd_data(nn).Y2./qpd_data(nn).SUM2,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 2 Y (V/V)')
+    
+        linkaxes([ax1 ax2 ax3 ax4],'x')
+        xlim([min(qpd_data(nn).t(ramp_start-buffer_t:mod_end+buffer_t)) max(qpd_data(nn).t(ramp_start-buffer_t:mod_end+buffer_t))])
+        ylim('auto')    
+        
+        % Plot modulation in um
+
+        fmsum_um=figure(401);
+        fmsum_um.Position = [600 100 800 600];
+        plotColor = jet(length(qpdfiles));
+    
+        ax1=subplot(4,3,1);
+        plot(qpd_data(nn).t,qpd_data(nn).X1./qpd_data(nn).SUM1*94.0311,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 1 X (um)')
+    
+        ax2=subplot(4,3,4);
+        plot(qpd_data(nn).t,qpd_data(nn).Y1./qpd_data(nn).SUM1,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 1 Y (V/V)')
+    
+        ax3=subplot(4,3,7);
+        plot(qpd_data(nn).t,qpd_data(nn).X2./qpd_data(nn).SUM2*994.9276,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 2 X (um)')
+    
+        ax4=subplot(4,3,10);
+        plot(qpd_data(nn).t,qpd_data(nn).Y2./qpd_data(nn).SUM2,'.',  'color',  plotColor(nn,:))
+        hold on;
+        xlabel('Time (ms)')
+        ylabel('ODT 2 Y (V/V)')
+    
+        linkaxes([ax1 ax2 ax3 ax4],'x')
+        xlim([min(qpd_data(nn).t(ramp_start-buffer_t:mod_end+buffer_t)) max(qpd_data(nn).t(ramp_start-buffer_t:mod_end+buffer_t))])
+        ylim('auto')    
+   
+    end
+
+    %%%%%%%%%%%%%% Summary plot for the modulation %%%%%%%%%%%%%%%
+
+    % (V/V)
+
+    figure(201)
+    ax5=subplot(4,3,[2 5]);
+    histogram([qpd_data.modfit_X1_A],15)
+    xlabel('Norm. X1 mod. amp. (V/V)')
+    
+    ax6=subplot(4,3,[3 6]);
+    histogram([qpd_data.modfit_X2_A],15)
+    xlabel('Norm. X2 mod. amp. (V/V)')
+    
+    ax7=subplot(4,3,[8 11]);
+    histogram([qpd_data.modfit_X1_B]/(2*pi),15)
+    hold on;
+    histogram([qpd_data.modfit_X2_B]/(2*pi),15)
+    xlabel('Phase/ (2$\pi$)','Interpreter','Latex','FontName','Helvetica')
+    
+    ax8=subplot(4,3,[9 12]);
+    histogram(1./[qpd_data.modfit_X1_T],15)
+    hold on;
+    histogram(1./[qpd_data.modfit_X2_T],15)
+    xlabel('Frequency (kHz)')
+
+    % um
+    figure(401)
+
+    % um Feb 13/24 conversion 94.0311 um/(V/V)
+    ax5=subplot(4,3,[2 5]);
+    histogram([qpd_data.modfit_X1_A]*94.0311,15)
+    xlabel('Norm. X1 mod. amp. (um)')
+    
+    % um Feb 13/24 conversion 994.9276 um/(V/V)
+    ax6=subplot(4,3,[3 6]);
+    histogram([qpd_data.modfit_X2_A]*994.9276,15)
+    xlabel('Norm. X2 mod. amp. (um)')
+    
+    ax7=subplot(4,3,[8 11]);
+    histogram([qpd_data.modfit_X1_B]/(2*pi),15)
+    hold on;
+    histogram([qpd_data.modfit_X2_B]/(2*pi),15)
+    xlabel('Phase/ (2$\pi$)','Interpreter','Latex','FontName','Helvetica')
+    
+    ax8=subplot(4,3,[9 12]);
+    histogram(1./[qpd_data.modfit_X1_T],15)
+    hold on;
+    histogram(1./[qpd_data.modfit_X2_T],15)
+    xlabel('Frequency (kHz)')
+    
+    %%%%%%%%%%%%% Summary plot for the power %%%%%%%%%%%%%
+    
+    fpsum = figure(301);
+    
+    axODT1A=subplot(3,4,1);
+    histogram([qpd_data.ODT1_ave],15)
+    xlabel('Sum 1 Ave. (V)')
+    
+    axODT1SD=subplot(3,4,2);
+    histogram([qpd_data.ODT1_std],15)
+    xlabel('Sum 1 Std. Dev. (V)')
+    
+    axODT2A=subplot(3,4,3);
+    histogram([qpd_data.ODT2_ave],15)
+    xlabel('Sum 2 Ave. (V)')
+    
+    axODT2SD=subplot(3,4,4);
+    histogram([qpd_data.ODT2_std],15)
+    xlabel('Sum 2 Std. Dev. (V)')
+    
+    axLXA=subplot(3,4,5);
+    histogram([qpd_data.XLatt_ave],15)
+    xlabel('X Latt. Ave. (V)')
+    
+    axLXSD=subplot(3,4,6);
+    histogram([qpd_data.XLatt_std],15)
+    xlabel('X Latt. Std. Dev. (V)')
+    
+    axLYA=subplot(3,4,7);
+    histogram([qpd_data.YLatt_ave],15)
+    xlabel('Y Latt. Ave. (V)')
+    
+    axLYSD=subplot(3,4,8);
+    histogram([qpd_data.YLatt_std],15)
+    xlabel('Y Latt. Std. Dev. (V)')
+    
+    axLZA=subplot(3,4,9);
+    histogram([qpd_data.ZLatt_ave],15)
+    xlabel('Z Latt. Ave. (V)')
+    
+    axLZSD=subplot(3,4,10);
+    histogram([qpd_data.ZLatt_std],15)
+    xlabel('Z Latt. Std. Dev. (V)')
+
+
+
+end
 
 
 end
