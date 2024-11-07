b3bf9d95fe27fd829d9e6eea658bc90ac1c313fc
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2024-11-07 01:41:43 -0500

    gather for single frequency heat

---
+++ analysis/_gatherScienceData/gather_2024_11_single_frequency_heat.m
@@ -0,0 +1,50 @@
+
+
+%% Saving and Output
+clear composite_data
+composite_data = struct;
+index=1;
+output_filename = '2024_11_shake_2.5Er_201.1G_single_tone';
+doUpload = 1;
+GDrive_root =['G:\My Drive\Lattice Shared\SharedData\Conductivity_Saturated_23-24'];
+
+%201.1 G high field 50 ms mod ramp 11/05 evap to 65.5 mW
+composite_data(index).Name = '201.1 G 11/06';
+composite_data(index).Runs= [ 
+       2024 11 06 18;
+       2024 11 06 19;
+       2024 11 06 20;
+       2024 11 07 01;
+    2024 11 07 02;
+
+    ];
+index=index+1;
+
+%% Redo Analysis
+do_redo_analysis = 0;    % Do you want to run analysis on it?
+
+if do_redo_analysis
+    opts=struct;
+    opts.do_ixon_main           = 1;   % ixon_main
+    opts.do_ixon_bin_analysis   = 1;   % ixon_bing
+    opts.do_ixon_dig_analysis   = 1;   % ixon_dig
+    ixon_super(composite_data,opts)
+end
+
+%% Gather Data
+composite_data = gatherCompositeData(composite_data);
+
+%% Upload the data
+
+ try
+     if ~exist(GDrive_root,'dir')
+     mkdir(GDrive_root);
+     end
+ end
+ 
+ if  doUpload && exist(GDrive_root,'dir')   
+     fprintf('upload to google drive ...');
+    gFile = fullfile(GDrive_root,[output_filename '.mat']);
+    save(gFile,'composite_data'); 
+    disp('done!')
+ end
--- analysis/_gatherScienceData/ixon_super.m
+++ analysis/_gatherScienceData/ixon_super.m
@@ -15,34 +15,41 @@ end
 if ~isfield(composite_opts,'do_ixon_dig_analysis')
     composite_opts.do_ixon_dig_analysis = 1;
 end
-
-for kk=1:length(composite_data)
-    dir_list = ixon_findRunDirectory(composite_data(kk).Runs);
+try
+for super_composite_data_index=1:length(composite_data)
+    dir_list = ixon_findRunDirectory(composite_data(super_composite_data_index).Runs);
     
-    for nn=1:length(dir_list)
+    for super_directory_index=1:length(dir_list)
         ixon_auto_dir = 0;
-        bin_auto_file = 0;
-        dig_auto_file = 0;
-        imgdir = dir_list{nn};
-
+        imgdir = dir_list{super_directory_index};
         if composite_opts.do_ixon_main
-            imgdir = dir_list{nn};
             ixon_main;
         end
 
+        bin_auto_file = 0;
+        imgdir = dir_list{super_directory_index};
         if composite_opts.do_ixon_bin_analysis
-            filename = fullfile(dir_list{nn},'figures','bindata.mat');
-            bin_imgdir = fullfile(dir_list{nn},'figures');
+            filename = fullfile(dir_list{super_directory_index},'figures','bindata.mat');
+            bin_imgdir = fullfile(dir_list{super_directory_index},'figures');
             ixon_bin_analysis;
         end
 
+        dig_auto_file = 0;
+        imgdir = dir_list{super_directory_index};
         if composite_opts.do_ixon_dig_analysis
-            dig_imgdir = fullfile(dir_list{nn},'figures');
-            filename = fullfile(dir_list{nn},'figures','digdata.mat');
+            dig_imgdir = fullfile(dir_list{super_directory_index},'figures');
+            filename = fullfile(dir_list{super_directory_index},'figures','digdata.mat');
             ixon_dig_analysis;
         end
     end   
 end
+
+catch ME
+
+    keyboard
+
+end
+
 
 
 end
