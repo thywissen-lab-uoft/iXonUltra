cfeccaf5677a9737a8562f7e34adfc1f06d39ebe
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2024-11-07 13:09:06 -0500

    Update gather_2024_11_single_frequency_heat.m

--- analysis/_gatherScienceData/gather_2024_11_single_frequency_heat.m
+++ analysis/_gatherScienceData/gather_2024_11_single_frequency_heat.m
@@ -17,11 +17,35 @@ composite_data(index).Runs= [
        2024 11 07 01;
     2024 11 07 02;
 
+
+    ];
+index=index+1;
+%% Saving and Output
+clear composite_data
+composite_data = struct;
+index=1;
+output_filename = '2024_11_shake_2.5Er_201.1G_single_tone_2';
+doUpload = 1;
+GDrive_root =['G:\My Drive\Lattice Shared\SharedData\Conductivity_Saturated_23-24'];
+
+%201.1 G high field 50 ms mod ramp 11/05 evap to 65.5 mW
+composite_data(index).Name = '201.1 G 11/06';
+composite_data(index).Runs= [ 
+    2024 11 07 03;
+    2024 11 07 04;
+    2024 11 07 05;
+    2024 11 07 06;
+    2024 11 07 07;
+    2024 11 07 08;
+    2024 11 07 09;
+    2024 11 07 10;
+    2024 11 07 11;
+
     ];
 index=index+1;
 
 %% Redo Analysis
-do_redo_analysis = 0;    % Do you want to run analysis on it?
+do_redo_analysis = 1;    % Do you want to run analysis on it?
 
 if do_redo_analysis
     opts=struct;
