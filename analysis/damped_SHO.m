f50c22693fca7da9dba13a3f4fd03e58203240f8
Cora <63414958+cjfujiwara@users.noreply.github.com>
2024-11-19 11:53:40 -0500

    updates

--- analysis/damped_SHO.m
+++ analysis/damped_SHO.m
@@ -45,14 +45,15 @@ m           = 40*amu;        % 40K mass
 omega0      = 2*pi*57;     % Natural trap Frequency in Hz [1/s]
 omegaD      = 2*pi*42;     % Trap frequency of XDT [1/s]
 tau         = 5e-3;        % Relaxation Time [s]
-T           = 10*tau;          % Ramp up time     [s]
+T           = 10*tau;      % Ramp up time     [s]
+% T           = 50e-3;
 
-gamma       = 1/tau
+gamma       = 1/tau;
 
 x0          = 1*1e-6;       % Drive amplitude [m]
 
 
-fvec = 2:2:120;
+fvec = 20:1:120;
 
 kB=1.380649e-23;  % boltzmman consntat [J/K]
 
@@ -82,50 +83,68 @@ for kk=1:length(fvec)
     power_curve = force_curve.*y(:,2);
     injected_energy(kk) = 1e9*trapz(dt,power_curve)/kB;
 
-    % 
-    % plot(t,y(:,1),'-','color',cc(kk,:));
-    % hold on
-
-
     Tdrive = 1/fvec(kk);
     Tinspect = t(end)-2*Tdrive;
+    Tinspect = T+0.1;
     ind = find(t>Tinspect,1);
-    tSub = t(ind:end);
-    xSub = y(ind:end,1);
+    
+    tSub{kk} = t(ind:end);
+    xSub{kk} = y(ind:end,1);
+
+    S=sum(sin(2*pi*fvec(kk)*tSub{kk}).*xSub{kk});
+    C=sum(cos(2*pi*fvec(kk)*tSub{kk}).*xSub{kk});
+
+
+    pp(kk)=max(y(:,1))-min(y(:,1));
+
     myfit = fittype(@(C,S,t) C*cos(2*pi*fvec(kk)*t)+S*sin(2*pi*fvec(kk)*t),'independent','t',...
         'coefficients',{'C','S'});
     opt=fitoptions(myfit);
-    opt.Start=[0 1];
-    fout = fit(tSub,xSub,myfit,opt);
+    opt.Start=[C S];
+
+    fout = fit(tSub{kk},xSub{kk},myfit,opt);
 
     Svec(kk) = fout.S;
     Cvec(kk) = fout.C;
-
     sigma_R(kk) = -fout.C*2*pi*fvec(kk)./(m*x0.*omegaD^2);
     sigma_I(kk) = -fout.S*2*pi*fvec(kk)./(m*x0.*omegaD^2);
-
 end
-% xlim([0 T+0.050]);
 
-% colormap(hF,cc);
-% % colorbar
-
-
-hF=figure
+hF=figure(20);
 hF.Color='w';
 clf(hF)
 
-subplot(121);
+subplot(141);
 plot(fvec,sigma_R/sigma0);
 hold on
 plot(fvec,sigma_I/sigma0);
 xlabel('frequency (Hz)');
 ylabel('conductivity (\sigma_0)')
 
-subplot(122);
+subplot(142);
 plot(fvec,injected_energy,'k-');
 ylabel('injected energy (nK)');
 xlabel('frequency (Hz)');
+
+subplot(143);
+plot(fvec,1e6*Cvec);
+hold on
+plot(fvec,1e6*Svec);
+xlabel('frequency (Hz)');
+ylabel('respone amplitude (\mum)');
+
+plot(fvec,-1e6*pp/2,'--');
+
+subplot(144);
+inds = [1:2:length(tSub)];
+c=turbo(length(inds));
+for kk=1:length(inds);
+    plot(tSub{inds(kk)},1e6*xSub{inds(kk)},'color',c(kk,:));
+    hold on
+end
+xlabel('time (ms)');
+ylabel('response (\mum)');
+
 %% Functions
 
 function x=XD(t)
@@ -134,7 +153,7 @@ function x=XD(t)
 end
 
     function [t,y]=timeEvolve(myomega,mytau)
-        tVec = 0:1e-4:(T+0.05);
+        tVec = 0:1e-4:(T+0.2);
         [t,y] = ode45(@foo,tVec,[0; 0]);
 
         % [t,y] = ode45(@foo,[0 T+0.05],[0; 0]);
