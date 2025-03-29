d67eedeb89dc46aa90bd99ea02b32cf6c68745a3
Cora <63414958+cjfujiwara@users.noreply.github.com>
2025-03-29 01:26:34 -0400

    Update dig_radialAnalysis.m

--- analysis/digital/dig_radialAnalysis.m
+++ analysis/digital/dig_radialAnalysis.m
@@ -127,11 +127,10 @@ sigma_r_numerical = r_expect*sqrt(2/pi); % expected r to sigma_R
 T_HOt = sigma2Tovert(sigma_r_numerical*aL);
 T_HO_nK = 1e9*sigma2Tovert(sigma_r_numerical*aL)*h*opts.Tunneling/kB;
 
-%% Fit Data to Gaussian PDf
+%% Fit Observations to 2D Gaussian PDF
 % Fit the measured atoms to a probability density function
-rMin = zeros(length(opts.GaussFitDensityMax),1);
-sigma_r_gauss_fit = zeros(length(opts.GaussFitDensityMax),1);
-sigma_r_gauss_fit_err = zeros(length(opts.GaussFitDensityMax),1);
+rMin                    = zeros(length(opts.GaussFitDensityMax),1);
+gauss_sigma             = zeros(length(opts.GaussFitDensityMax),2);
 
 % Numerical integral of n(r)
 I0=trapz([0 rcen],[mean(nr_mean(1:2)); nr_mean]);
@@ -149,7 +148,7 @@ for nn=1:length(opts.GaussFitDensityMax)
         Iouter(nn) = trapz(rcen(iMin:end),nr_mean(iMin:end)); % partial integral
     end
     % Fit the distribution
-    [sigma_r_gauss_fit(nn),sigma_r_gauss_fit_err(nn)]=pdf_gauss_fit(r,rMin(nn));
+    [gauss_sigma(nn,1),gauss_sigma(nn,2)]=pdf_gauss_fit(r_all,rMin(nn));
 end
 
 % Convert the fit into n(r)
@@ -273,7 +272,7 @@ n2_lim(2) = min([n2_lim(2) n2(end)]);
     output.Gibbs_Rs                 = GibbsFit.s*aL_um; % in um
     output.Gibbs_RsErr              = GsErr*aL_um;      % in um
 
-%% Plotting
+%% Initialize Figure
 
     if ~isfield(opts,'Parent')
         opts.Parent = figure('color','w','Position',[300 100 1200 630],...
@@ -297,14 +296,13 @@ n2_lim(2) = min([n2_lim(2) n2(end)]);
         tstr  = digdata.FileNames{1};
     end    
     
-        tFig=uicontrol('style','text','string',tstr,...
-            'units','pixels','backgroundcolor',...
-            'w','horizontalalignment','left','parent',hF);
-        tFig.Position(4)=tFig.Extent(4);
-        tFig.Position(3)=tFig.Extent(3);
-        tFig.Position(1:2)=[1 1];
-    
-    % 2D Image of Average Image
+    tFig=uicontrol('style','text','string',tstr,...
+        'units','pixels','backgroundcolor',...
+        'w','horizontalalignment','left','parent',hF);
+    tFig.Position(4)=tFig.Extent(4);
+    tFig.Position(3)=tFig.Extent(3);
+    tFig.Position(1:2)=[1 1];
+   %%  2D Image of Average Image
     hF.UserData.Axes{1}=subplot(1,2,1,'parent',hF);
     ax1= hF.UserData.Axes{1};
     ax1.UserData.subplot_inds = [1 2 1];
@@ -363,7 +361,8 @@ n2_lim(2) = min([n2_lim(2) n2(end)]);
     ax2.Position = [x0 y0 w h];
 
 
-    histogram('BinEdges',redges,'BinCounts',nr_mean)
+    pData=histogram('BinEdges',redges,'BinCounts',nr_mean)
+    legStr ={'data'};
     hold on
     errorbar(rcen,nr_mean,nr_std,'linewidth',1,'markerfacecolor',[.5 .5 .5],...
         'parent',ax2,'linestyle','none','color','k');
@@ -395,22 +394,22 @@ n2_lim(2) = min([n2_lim(2) n2(end)]);
 
     rVec=linspace(0,100,100);
     for nn=1:length(opts.GaussFitDensityMax)
-        s=sigma_r_gauss_fit(nn);
-        s_err=sigma_r_gauss_fit_err(nn);
+        s       = gauss_sigma(nn,1);
+        s_err   = gauss_sigma(nn,2);
 
         N = Iouter(nn)/nr_partial_cdf(s,rMin(nn));
         pGaussFits(nn)=plot(rVec,nr_fit(s,rVec,N),'-');
-        legStr{nn}=['$' num2str(s*aL_um,'%.1f')  ...
-            '(' num2str(round(10*s_err*aL_um)) ')~\mu\mathrm{m}$' ...
-            ' $n<' num2str(opts.GaussFitDensityMax(nn)) '$'];
+        legStr{end+1}=['2D Gauss : $\sigma=' num2str(round(s),'%.1f')  ...
+             '~sites (' num2str(round(s*aL_um,1)) '~\mu \mathrm{m}' ...
+            ' n<' num2str(opts.GaussFitDensityMax(nn)) '$'];
     end
 
     % Plot gibbs fit
-    pGaussFits(nn+1) = plot(rVec,feval(GibbsFit,rVec),'-');
-    legStr{nn+1} = ['Gibbs $z_0 = ' num2str(GibbsFit.z0,'%.1f') '$, $T= ' num2str(T_HOt_g,'%.1f') 't$ ' ...
+    pGaussFits(end+1) = plot(rVec,feval(GibbsFit,rVec),'-');
+    legStr{end+1} = ['Gibbs $z_0 = ' num2str(GibbsFit.z0,'%.1f') '$, $T= ' num2str(T_HOt_g,'%.1f') 't$ ' ...
         '(' num2str(T_HO_g_nK,'%.0f') ' nK)' ];
     if ~isempty(opts.GaussFitDensityMax)
-    legend(pGaussFits,legStr,'interpreter','latex','fontsize',8,...
+    legend([pData pGaussFits],legStr,'interpreter','latex','fontsize',8,...
         'location','southeast','parent',hF);
     end
 
@@ -454,12 +453,25 @@ n2_lim(2) = min([n2_lim(2) n2(end)]);
   
 end
 
+function [sigmaR,sigmaR_err] = pdf_gauss2D(r)
+    % Probability density function
+    pdf_r = @(r,sigma) (2*pi*r.*exp(-r.^2./(2*sigma.^2))./(2*pi*sigma.^2)).*[r>0];
+    % Cummulate density function (goes from 0 to 1);
+    cdf_r = @(r,sigma) 1-exp(-r.^2/(2*sigma^2));
+    [pdf_r_vals,pdf_r_cints] = mle(r,...
+        'pdf',pdf_r,'cdf',cdf_r,...
+        'Start',mean(r,'all')*sqrt(2/pi), ...
+        'LowerBound',0);
+    sigmaR = pdf_r_vals(1);
+    sigmaR_err =(pdf_r_cints(2,1)-pdf_r_cints(1,1))*0.5;
+end
+
 function [sigmaR,sigmaR_err]=pdf_gauss_fit(r,rMin)
 
     % Probability density function
-    pdf_r = @(r,sigma) 2*pi*r.*exp(-r.^2./(2*sigma.^2))./(2*pi*sigma.^2);
+    pdf_r = @(r,sigma) (2*pi*r.*exp(-r.^2./(2*sigma.^2))./(2*pi*sigma.^2)).*[r>0];
     % Cummulate density function (goes from 0 to 1);
-    cdf_r = @(r,sigma) 1-exp(-r.^2/(2*sigma^2));
+    cdf_r = @(r,sigma) (1-exp(-r.^2/(2*sigma^2))).*[r>0];
 
 
     r_expect = mean(r,'all');   % expected r
@@ -478,6 +490,7 @@ function [sigmaR,sigmaR_err]=pdf_gauss_fit(r,rMin)
         'Start',r_expect*sqrt(2/pi), ...
         'LowerBound',0);
     end    
+    
     sigmaR = pdf_r_vals(1);
     sigmaR_err =(pdf_r_cints(2,1)-pdf_r_cints(1,1))*0.5;
 end
