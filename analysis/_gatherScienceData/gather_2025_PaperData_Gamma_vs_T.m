1a83cf46770e197d057daf9b1557e210088c607e
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2025-05-14 15:09:52 -0400

    gamma vs T upload edits

--- analysis/_gatherScienceData/gather_2025_PaperData_Gamma_vs_T.m
+++ analysis/_gatherScienceData/gather_2025_PaperData_Gamma_vs_T.m
@@ -1,6 +1,6 @@
 clear composite_data
 composite_data = struct;
-
+warning off
 %% T = 1t (201.1G) 
 runs = [  
     2024 11 08 19;
@@ -12,7 +12,7 @@ runs = [
     2024 11 08 25;
     2024 11 08 26;
     2024 11 09 01;
-    2024 11 09 02;
+    2024 11 09 02; % small signal
     2024 11 09 03;
     2024 11 09 04;
     2024 11 09 05;
@@ -28,52 +28,52 @@ composite_data(end+1).Runs = runs;
 composite_data(end).Name = '2024_11_08 201.1 G spectra T = 1t';
 %% T = 1t (201.1G) 
 runs = [  
-    2024 12 05 01;
-    2024 12 05 02;
-    2024 12 05 03;
-    2024 12 05 04;
-    2024 12 05 05;
-    2024 12 06 01;
-    2024 12 06 02;
-    2024 12 06 03;
-    2024 12 06 04;
-    2024 12 06 05;
-    2024 12 06 06;
-    2024 12 06 07;
-    2024 12 06 08;
-    2024 12 06 09;
-    2024 12 06 10;
-    2024 12 06 11;
-    2024 12 06 12;
+%     2024 12 05 01; % plane selection issues? inconsistent density
+    2024 12 05 02; % increasing atom number and density, but overall OK
+    2024 12 05 03; % OK, fluctuating atom number
+    2024 12 05 04; % OK, a few bad shots
+    2024 12 05 05; % OK
+    2024 12 06 01; % OK
+    2024 12 06 02; % inconsistent/low density
+    2024 12 06 03; % OK
+    2024 12 06 04; % OK
+    2024 12 06 05; % OK
+    2024 12 06 06; % a few shots of low density/atom number
+    2024 12 06 07; % inconsistent/low density
+    2024 12 06 08; % OK
+    2024 12 06 09; % decreasing density
+    2024 12 06 10; % OK
+%     2024 12 06 11; % inconsistent/low density & low amplitude response
+    2024 12 06 12; 
     ];
 
 composite_data(end+1).Runs = runs;
-composite_data(end).Name = '2024_12_08 201.1 G spectra T = 1t';
+composite_data(end).Name = '2024_12_05 201.1 G spectra T = 1t';
 %% T = 2.9t (201.1G) 
 runs = [  
-    2024 12 09 02;
-    2024 12 09 03;
-    2024 12 09 04;
-    2024 12 09 05;
-    2024 12 09 06;
-    2024 12 10 01;
-    2024 12 10 02;
-    2024 12 10 03;
-    2024 12 10 04;
-    2024 12 10 05;
-    2024 12 10 06;
-    2024 12 10 07;
-    2024 12 10 08;
-    2024 12 10 09;
-    2024 12 10 10;
-    2024 12 10 11;
-    2024 12 10 12;
-    2024 12 10 13;
-    2024 12 10 14;
+    2024 12 09 02; % OK
+    2024 12 09 03; % OK
+    2024 12 09 04; % fluctuating density, low amplitude response/bad fit
+%     2024 12 09 05; % decreasing density, slightly out of focus, bad fit
+    2024 12 09 06; % not great signal, but OK
+%     2024 12 10 01; % plane selection issues? inconsistent density, bad fit
+    2024 12 10 02; % not great fit
+    2024 12 10 03; % OK
+    2024 12 10 04; % low density, a few bad shots
+    2024 12 10 05; % OK
+    2024 12 10 06; % OK
+    2024 12 10 07; % OK
+    2024 12 10 08; % OK
+    2024 12 10 09; % OK
+%     2024 12 10 10; % bad fit, inconsistent density
+    2024 12 10 11; % OK
+    2024 12 10 12; % OK
+    2024 12 10 13; % OK
+    2024 12 10 14; % OK
     ];
 
 composite_data(end+1).Runs = runs;
-composite_data(end).Name = '2024_12_08 201.1 G spectra T = 2.9t';
+composite_data(end).Name = '2024_12_09 201.1 G spectra T = 2.9t';
 
 %% T = 1.7t (201.1G) 
 runs = [  
@@ -85,12 +85,12 @@ runs = [
     2025 01 23 12;
     2025 01 24 01;
     2025 01 24 02;
-    2025 01 24 03;
+%     2025 01 24 03; % no signal
     2025 01 24 04;
     2025 01 24 05;
-    2025 01 24 06;
-    2025 01 24 07;
-    2025 01 24 08;
+%     2025 01 24 06; % no signal, low density
+    2025 01 24 07; % OK, but slightly out of focus
+    2025 01 24 08; % not the best fit, slightly out of focus
     2025 01 24 09;
     2025 01 24 10;
     2025 01 24 11;
@@ -100,54 +100,20 @@ runs = [
 composite_data(end+1).Runs = runs;
 composite_data(end).Name = '2025_01_23 201.1 G spectra T = 1.7t';
 
-%% T = 3t (201.1G) spectrum looks terrible
-% runs = [  
-%     2025 01 24 15;
-%     2025 01 24 16;
-%     2025 01 24 17;
-%     2025 01 24 18;
-%     2025 01 24 20;
-%     2025 01 24 21;
-%     2025 01 24 22;
-%     2025 01 25 01;
-%     2025 01 25 02;
-%     2025 01 25 03;
-%     2025 01 25 04;
-%     2025 01 25 05;
-%     2025 01 25 06;
-%     2025 01 25 07;
-%     2025 01 25 09;
-%     2025 01 25 10;
-%     2025 01 25 11;
-%     2025 01 25 12;
-%     2025 01 25 13;
-%     2025 01 25 14;
-%     2025 01 25 15;
-%     2025 01 25 16;
-%     2025 01 25 17;
-%     2025 01 25 18;
-%     2025 01 25 19;
-%     2025 01 25 20;
-%     2025 01 25 21;
-%     2025 01 25 22;
-%     ];
-% 
-% composite_data(end+1).Runs = runs;
-% composite_data(end).Name = '2025_01_24 201.1 G spectra T = 3t';
-
 %% T = 0.93t (201.1G) 
 runs = [  
     2025 01 25 25;
     2025 01 25 26;
-    2025 01 25 27;
-    2025 01 26 01;
-    2025 01 26 02;
+%     2025 01 25 27; % no signal
+    2025 01 26 01; 
+    2025 01 26 02; 
     2025 01 26 03;
     2025 01 26 04;
-    2025 01 26 05;
-    2025 01 26 06;
+    2025 01 26 05; % not the best signal, slightly higher density
+    2025 01 26 06; 
     2025 01 26 07;
-    2025 01 26 09;
+    2025 01 26 08; % this is good, not sure why it was removed previously
+    2025 01 26 09; 
     2025 01 26 10;
     2025 01 26 11;
     2025 01 26 12;
@@ -158,12 +124,12 @@ runs = [
     2025 01 26 17;
     2025 01 26 18;
     2025 01 26 19;
-    2025 01 26 20;
-    2025 01 26 21;
-    2025 01 26 22;
-    2025 01 26 23;
-    2025 01 26 24;
-    2025 01 26 25;
+    2025 01 26 20; % not great signal, fluctuating density
+%     2025 01 26 21; % out of focus, binning & digitization is wrong
+%     2025 01 26 22; % out of focus, binning & digitization is wrong
+%     2025 01 26 23; % out of focus, binning & digitization is wrong
+    2025 01 26 24; % back in focus
+    2025 01 26 25; 
     2025 01 26 26;
     ];
 
@@ -174,19 +140,19 @@ composite_data(end).Name = '2025_01_25 201.1 G spectra T = 0.93t';
 runs = [  
     2025 03 15 31;
     2025 03 15 32;
-    2025 03 16 01;
-    2025 03 16 02;
+    2025 03 16 01; % decreasing density, no the best signal
+    2025 03 16 02; 
     2025 03 16 03;
     2025 03 16 04;
-    2025 03 16 05;
-    2025 03 16 06;
+    2025 03 16 05; % low density
+    2025 03 16 06; 
     2025 03 16 07;
-    2025 03 16 08;
-    2025 03 16 09;
+    2025 03 16 08; % not the best signal, heating, decreasing density
+%     2025 03 16 09; % many bad shots
     2025 03 16 10;
     2025 03 16 11;
     2025 03 16 12;
-    2025 03 16 13;
+    2025 03 16 13; % low density
     ];
 
 composite_data(end+1).Runs = runs;
@@ -243,8 +209,8 @@ composite_data = gatherCompositeData(composite_data);
 
 %% Upload
 
-doUpload = true;
-
+% doUpload = true;
+doUpload = false;
 
 GDrive_root =['G:\My Drive\Lattice Shared\SharedData\Conductivity_Saturated_23-24'];
 output_folder_name ='2025_paper_data_vs_T';
